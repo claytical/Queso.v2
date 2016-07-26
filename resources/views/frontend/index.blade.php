965e988d84e631f50313b293c4d25e5341affb1b
Clay <c.ewing@miami.edu>
2016-07-25 20:51:13 -0400

    vanilla splash page for index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,4 +1,4 @@
-@extends('frontend.layouts.master')
+@extends('frontend.layouts.guest')
 
 @section('content')
     <div class="row">
@@ -14,34 +14,6 @@
                     {{ trans('strings.frontend.welcome_to', ['place' => app_name()]) }}
                 </div>
             </div><!-- panel -->
-
-        </div><!-- col-md-10 -->
-
-
-        @permission('view-backend')
-            <div class="col-md-10 col-md-offset-1">
-
-                <div class="panel panel-default">
-                    <div class="panel-heading"><i class="fa fa-home"></i> {{ trans('strings.frontend.tests.based_on.permission') . trans('strings.frontend.tests.using_access_helper.permission_name') }}</div>
-
-                    <div class="panel-body">
-                        Backend User
-                    </div>
-                </div><!-- panel -->
-
-            </div><!-- col-md-10 -->
-        @endauth
-
-
-        <div class="col-md-10 col-md-offset-1">
-
-        </div><!-- col-md-10 -->
-
-        <div class="col-md-10 col-md-offset-1">
-
-        </div><!-- col-md-10 -->
-
-        <div class="col-md-10 col-md-offset-1">
 
         </div><!-- col-md-10 -->
 
