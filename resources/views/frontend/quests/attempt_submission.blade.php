8a6e7415125b40d0b305d5f6b6f1c9c08c6b2d24
Clay <c.ewing@miami.edu>
2016-07-26 00:06:46 -0400

    attempt submission

---
+++ resources/views/frontend/quests/attempt_submission.blade.php
@@ -0,0 +1,27 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+<h2>Quest Name</h2>
+
+        <div class="col-lg-12">
+            <div class="row">
+                <h4>Points Available</h4>
+                <h6>Due 00/00/000</h6>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+                {!! Form::open(array('url' => 'foo/bar')) !!}
+        
+                {!! Form::textarea('notes', null, ['class' => 'field', 'files' => true]) !!}
+                {!! Form::file('image') !!}
+                {!! Form::submit('Submit') !!}
+
+                {!! Form::close() !!}
+            </div>
+            
+        </div>
+
+@endsection
+
+@section('after-scripts-end')
+    <script>
+    </script>
+@stop
