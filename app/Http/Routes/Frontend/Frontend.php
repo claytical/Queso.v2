43a779f3014d547167d2b650cf506e650846300e
Clay <c.ewing@miami.edu>
2016-07-25 20:26:07 -0400

    changing dashboard

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -10,8 +10,8 @@ Route::get('/', 'FrontendController@splash')->name('frontend.splash');
  */
 Route::group(['middleware' => 'auth'], function () {
     Route::group(['namespace' => 'User'], function() {
-		Route::get('/', 'DashboardController@welcome')->name('frontend.welcome');
-        Route::get('dashboard', 'DashboardController@index')->name('frontend.user.dashboard');
+//		Route::get('/', 'DashboardController@welcome')->name('frontend.welcome');
+        Route::get('/dashboard', 'DashboardController@index')->name('frontend.user.dashboard');
         Route::get('profile/edit', 'ProfileController@edit')->name('frontend.user.profile.edit');
         Route::patch('profile/update', 'ProfileController@update')->name('frontend.user.profile.update');
     });
