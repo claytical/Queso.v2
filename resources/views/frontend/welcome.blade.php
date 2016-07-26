ebc4d8afe24507e37ea82c1268ccf5b93ae3a546
Clay <c.ewing@miami.edu>
2016-07-25 21:44:19 -0400

    better placeholders for dashboard

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -4,22 +4,55 @@
    <div class="row">
         <div class="col-lg-12">
             <div class="col-lg-6">
-            Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Donec sed odio dui.
+                <div class="panel panel-default">
+                  <div class="panel-heading">Peer Feedback Requests</div>
+                  <div class="panel-body">
+                    <h4>Quest Name</h4>
 
-Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas faucibus mollis interdum. Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis.
+                    <ul class="list-unstyled">
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                    </ul>
 
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo. Donec sed odio dui.
+                    <h4>Quest Name</h4>
 
-Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</div>
+                    <ul class="list-unstyled">
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                    </ul>
+
+                    <h4>Quest Name</h4>
+
+                    <ul class="list-unstyled">
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                        <li>Student Name</li>
+                    </ul>
+
+                  </div>
+                </div>            
+            </div>
 
             <div class="col-lg-6">
-Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+                <div class="panel panel-default">
+                  <div class="panel-heading">Class Name</div>
+                  <div class="panel-body">
+                    <h4>Instructor: John Doe</h4>
+                    <h4>Class Time: Wednesdays at 3pm</h4>
+                    <h4>Current Level: B+ (89/100)</h4>
+                </div>
 
-Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus.
-
-Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum.
-
-Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo.
+                <div class="panel panel-default">
+                  <div class="panel-heading">Notifications</div>
+                  <div class="panel-body">
+                    <ul class="list-unstyled">
+                        <li>Quest was graded</li>
+                        <li>You got feedback on something</li>
+                        <li>Someone did something</li>
+                    </ul>                
+                  </div>
             </div>
 
         </div>
