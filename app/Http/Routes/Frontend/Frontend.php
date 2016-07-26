990f036f086da2cb11932ecbfb6537e3597ed762
Clay <c.ewing@miami.edu>
2016-07-25 20:42:17 -0400

    changing routes

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -3,14 +3,14 @@
 /**
  * Frontend Controllers
  */
-Route::get('/', 'FrontendController@splash')->name('frontend.index');
+Route::get('/', 'FrontendController@splash')->name('frontend.splash');
 
 /**
  * These frontend controllers require the user to be logged in
  */
 Route::group(['middleware' => 'auth'], function () {
     Route::group(['namespace' => 'User'], function() {
-        Route::get('/dashboard', 'DashboardController@index')->name('frontend.user.dashboard');
+        Route::get('/dashboard', 'DashboardController@index')->name('frontend.user.index');
         Route::get('profile/edit', 'ProfileController@edit')->name('frontend.user.profile.edit');
         Route::patch('profile/update', 'ProfileController@update')->name('frontend.user.profile.update');
     });
