e82232edbdb05e2d76eb6946bb83897d2ff3ce7c
Clay <c.ewing@miami.edu>
2016-07-26 23:06:26 -0400

    available quests layout

--- resources/views/frontend/quests/attempt_submission.blade.php
+++ resources/views/frontend/quests/attempt_submission.blade.php
@@ -1,23 +1,30 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-<h2>Quest Name</h2>
-
         <div class="col-lg-12">
             <div class="row">
-                <h4>Points Available</h4>
-                <h6>Due 00/00/000</h6>
-                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-                {!! Form::open(array('url' => 'quest/submit', 'class' => '', 'id' => 'no-ajax-upload')) !!}
-        
-                {!! Form::textarea('notes', null, ['class' => 'field', 'files' => true]) !!}
+                <div class="col-lg-9">
+                    <h2>Quest Name</h2>
+                </div>
+                <div class="col-lg-3">
+                    <h4>Points Available</h4>
+                    <h6>Due 00/00/000</h6>
+                </div>
+                <div class="col-lg-9">
+                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+                </div>
+                <div class="col-lg-3">
+                    <button type="button" class="btn btn-primary btn-lg">Submit</button>
+                </div>
 
-<!--                <input type="file" name="file" multiple /> -->
-                {!! Form::submit('Submit') !!}
-
-                {!! Form::close() !!}
-                {!! Form::open(['url' => 'dropzone/uploadFiles', 'class' => 'dropzone', 'files'=>true, 'id'=>'my-awesome-dropzone']) !!}
-
+                <div class="col-lg-12">
+                    {!! Form::open(array('url' => 'quest/submit', 'class' => '', 'id' => 'no-ajax-upload')) !!}
+                    {!! Form::textarea('notes', null, ['class' => 'field', 'files' => true]) !!}
+                    {!! Form::close() !!}
+                </div>
+                <div class="col-lg-12">
+                    {!! Form::open(['url' => 'dropzone/uploadFiles', 'class' => 'dropzone', 'files'=>true, 'id'=>'my-awesome-dropzone']) !!}
+                </div>
             </div>
             
         </div>
