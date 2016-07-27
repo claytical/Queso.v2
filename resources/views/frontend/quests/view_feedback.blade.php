126fea1b5bb43d41c67d3cde674083ad4c862653
Clay <c.ewing@miami.edu>
2016-07-27 14:17:46 -0400

    reformat

--- resources/views/frontend/quests/view_feedback.blade.php
+++ resources/views/frontend/quests/view_feedback.blade.php
@@ -15,8 +15,8 @@
         </div>
     </div>
     <div class="row">
-        <h4>What Your Peers Liked</h4>
             <div class="col-lg-11">
+                <h4>What Your Peers Liked</h4>
                 <h6>Student Name</h6>
                 <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
             </div>
@@ -36,35 +36,37 @@
     </div>
 
     <div class="row">
-        <h4>Suggestions From Your Peers</h4>
-        
-            <div class="col-lg-11">
-                <h6>Student Name</h6>
-                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-            </div>
-
-            <div class="col-lg-1">
-                <a class="btn btn-default pull-right" href="#" role="button">Like</a>
-            </div>
-
-            <div class="col-lg-11">
-                <h6>Student Name</h6>
-                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-            </div>
-
-            <div class="col-lg-1">
-                <a class="btn btn-default pull-right" href="#" role="button">Like</a>
-            </div>
-
-    <div class="row">
-        <h4>From The Professor</h4>
             <div class="col-lg-12">
+                <h4>Suggestions From Your Peers</h4>
+            </div>    
 
+            <div class="col-lg-11">
+                <h6>Student Name</h6>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+            </div>
+
+            <div class="col-lg-1">
+                <a class="btn btn-default pull-right" href="#" role="button">Like</a>
+            </div>
+
+            <div class="col-lg-11">
+                <h6>Student Name</h6>
+                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+            </div>
+
+            <div class="col-lg-1">
+                <a class="btn btn-default pull-right" href="#" role="button">Like</a>
+            </div>
+    </div>
+    
+    <div class="row">
+            <div class="col-lg-12">
+                <h4>From The Professor</h4>
                 <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
             </div>
  
-         </div>
     </div>
+
 @endsection
 
 @section('after-scripts-end')
