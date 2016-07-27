db2d0a58dcdb73f1341c0f468ddc06cbac12681f
Clay <c.ewing@miami.edu>
2016-07-27 17:53:40 -0400

    adding panel

--- resources/views/frontend/manage/quests.blade.php
+++ resources/views/frontend/manage/quests.blade.php
@@ -2,14 +2,7 @@
 
 @section('content')
 <div class="col-lg-12">
-    <div class="row">
-        <div class="col-lg-9">
-            <h2>Quests</h2>
-        </div>
-        <div class="col-lg-3">
-            <a href="#" class="btn btn-primary btn-lg">New Quest</a>
-        </div>
-    </div>
+    <h2>Quests <a href="#" class="btn btn-primary btn-lg pull-right">New Quest</a></h2>
 </div>
 
 <div class="col-lg-12">
@@ -55,42 +48,48 @@
     </div>
 
     <div class="col-lg-3">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Category <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu">
-                    <li><a href="#" class="sort" data-sort="submission">Prototypes</a></li>
-                    <li><a href="#" class="sort" data-sort="date">Research Papers</a></li>
-                  </ul>
-                </div>              
-            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Filter Options</div>
+                        <div class="panel-body">
+                            <div class="btn-group btn-block">
+                              <button type="button" class="btn-block btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Category <span class="caret"></span>
+                              </button>
+                              <ul class="dropdown-menu">
+                                <li><a href="#" class="sort" data-sort="submission">Prototypes</a></li>
+                                <li><a href="#" class="sort" data-sort="date">Research Papers</a></li>
+                              </ul>
+                            </div>   
+                                <h5>Requirements</h5>
+                                    <form class="form-horizontal">
+                                      <div class="form-group">
+                                        <label for="skill1" class="col-sm-2 control-label">Skill #1</label>
+                                        <div class="col-sm-10">
+                                          <input type="text" class="form-control" id="skill1">
+                                        </div>
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="skill2" class="col-sm-2 control-label">Skill #2</label>
+                                        <div class="col-sm-10">
+                                          <input type="text" class="form-control" id="skill2">
+                                        </div>
+                                      </div>
 
-        <h5>Requirements</h5>
-        <form class="form-horizontal">
-          <div class="form-group">
-            <label for="skill1" class="col-sm-2 control-label">Skill #1</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="skill1">
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="skill2" class="col-sm-2 control-label">Skill #2</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="skill2">
-            </div>
-          </div>
+                                      <div class="form-group">
+                                        <label for="skill2" class="col-sm-2 control-label">Skill #2</label>
+                                        <div class="col-sm-10">
+                                          <input type="text" class="form-control" id="skill2">
+                                        </div>
+                                      </div>
 
-          <div class="form-group">
-            <label for="skill2" class="col-sm-2 control-label">Skill #2</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="skill2">
-            </div>
-          </div>
+                                      <button class="btn btn-default btn-block">Filter</button>
 
-          <button class="btn btn-default">Filter</button>
+                                    </form> 
 
-        </form> 
+
+
+                        </div>
+                </div>
 
     </div>
 
