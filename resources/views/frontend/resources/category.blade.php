32e7f8f8f75ce97bd213dc1ac85c8de5da76f4ff
Clay <c.ewing@miami.edu>
2016-07-27 13:40:49 -0400

    changing resource category placeholder

--- resources/views/frontend/resources/category.blade.php
+++ resources/views/frontend/resources/category.blade.php
@@ -11,31 +11,42 @@
                 <div class="col-lg-12">
                     <h4>Resource Name</h4>
                     <h5>Posted 00/00/000</h5>
+                    <p>Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.</p>
                     <h6>Attached Files</h6>
                     <a href="#" class="btn btn-default">filename.pdf</a>         
                     <a href="#" class="btn btn-default">filename.pdf</a>
                     <a href="#" class="btn btn-default">filename.pdf</a>
-                    EMBEDLY CARD GOES HERE
+                    <div class="row">
+                        EMBEDLY CARD GOES HERE
+                    </div>
+
+                </div>
+
+                <div class="col-lg-12">
+                    <h4>Resource Name</h4>
+                    <h5>Posted 00/00/000</h5>
+                    <p>Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.</p>
+                    <h6>Attached Files</h6>
+                    <a href="#" class="btn btn-default">filename.pdf</a>         
+                    <a href="#" class="btn btn-default">filename.pdf</a>
+                    <a href="#" class="btn btn-default">filename.pdf</a>
+                    <div class="row">
+                        EMBEDLY CARD GOES HERE
+                    </div>
+
                 </div>
 
                 <div class="col-lg-12">
                     <h4>Resource Name</h4>
                     <h5>Posted 00/00/000</h5>
                     <h6>Attached Files</h6>
+                    <p>Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum.</p>
                     <a href="#" class="btn btn-default">filename.pdf</a>         
                     <a href="#" class="btn btn-default">filename.pdf</a>
                     <a href="#" class="btn btn-default">filename.pdf</a>
-                    EMBEDLY CARD GOES HERE
-                </div>
-
-                <div class="col-lg-12">
-                    <h4>Resource Name</h4>
-                    <h5>Posted 00/00/000</h5>
-                    <h6>Attached Files</h6>
-                    <a href="#" class="btn btn-default">filename.pdf</a>         
-                    <a href="#" class="btn btn-default">filename.pdf</a>
-                    <a href="#" class="btn btn-default">filename.pdf</a>
-                    EMBEDLY CARD GOES HERE
+                    <div class="row">
+                        EMBEDLY CARD GOES HERE
+                    </div>
                 </div>
             
             </div>
