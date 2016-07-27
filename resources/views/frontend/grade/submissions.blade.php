ceedf0c620f74a5392ac0373cdfd95aaa0ee9a05
Clay <c.ewing@miami.edu>
2016-07-27 16:26:32 -0400

    list test

--- resources/views/frontend/grade/submissions.blade.php
+++ resources/views/frontend/grade/submissions.blade.php
@@ -6,23 +6,30 @@
         <div class="col-lg-12">
             <div id="submission-list">
               <input class="search" placeholder="Search" />
-                  <button class="sort" data-sort="name">
-                    Sort
+                  <button class="sort" data-sort="submission">
+                    Sort by Quest Name
                   </button>
+                  <button class="sort" data-sort="date">
+                    Sort by Date
+                  </button>
+                  <button class="sort" data-sort="student">
+                    Sort by Student Name
+                  </button>
+
                 <ul class="list-unstyled list">
                     <li>
                         <h4 class="submission">{{ link_to('quest/1/attempt/submission', 'Submission Quest') }}</h4>                  
-                        <p>Submitted <span class="date">00/00/0000</span> by <span class="student">Student Name</span></p>
+                        <p>Submitted <span class="date">01/12/2016</span> by <span class="student">Gloria</span></p>
                     </li>
 
                     <li>
                         <h4 class="submission">{{ link_to('quest/1/attempt/submission', 'Link Quest') }}</h4>                  
-                        <p>Submitted <span class="date">00/00/0000</span> by <span class="student">Student Name</span></p>
+                        <p>Submitted <span class="date">01/12/2015</span> by <span class="student">Sauron</span></p>
                     </li>
 
                     <li>
                         <h4 class="submission">{{ link_to('quest/1/attempt/submission', 'Feedback Quest') }}</h4>                  
-                        <p>Submitted <span class="date">00/00/0000</span> by <span class="student">Student Name</span></p>
+                        <p>Submitted <span class="date">03/01/2016</span> by <span class="student">Adrian</span></p>
                     </li>
 
                 </ul>
