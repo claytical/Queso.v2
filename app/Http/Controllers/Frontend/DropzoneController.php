99f33e611fde937566baaffe791ba8d0fb0c0201
Clay <c.ewing@miami.edu>
2016-07-26 21:41:24 -0400

    removing Input class

--- app/Http/Controllers/Frontend/DropzoneController.php
+++ app/Http/Controllers/Frontend/DropzoneController.php
@@ -21,22 +21,11 @@ class DropzoneController extends Controller
 
 public function uploadFiles(Request $request) {
  
-        $input = $request->all();
- 
-        $rules = array(
-            'file' => 'image|max:3000',
-        );
- 
-        $validation = Validator::make($input, $rules);
- 
-        if ($validation->fails()) {
-            return Response::make($validation->errors->first(), 400);
-        }
- 
+
         $destinationPath = 'uploads'; // upload path
-        $extension = Input::file('file')->getClientOriginalExtension(); // getting file extension
+        $extension = $request->file('file')->getClientOriginalExtension(); // getting file extension
         $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
-        $upload_success = Input::file('file')->move($destinationPath, $fileName); // uploading file to given path
+        $upload_success = $request->file('file')->move($destinationPath, $fileName); // uploading file to given path
  
         if ($upload_success) {
             return Response::json('success', 200);
