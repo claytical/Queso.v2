7dac90d6fdd2593aa04eaa3bcd2f7474ae16db66
Clay <c.ewing@miami.edu>
2016-07-28 17:01:06 -0400

    clone adjustments

--- resources/views/frontend/manage/clonequest.blade.php
+++ resources/views/frontend/manage/clonequest.blade.php
@@ -11,7 +11,7 @@
     <div id="quest_name">
         <div class="row">
             <div class="col-lg-12">
-                    {!! Form::open(['url' => 'manage/quest/create', 'id'=>'quest-create-form']) !!}
+                    {!! Form::open(['url' => 'manage/quest/clone', 'id' => 'quest-clone-form']) !!}
 
                 <h3>What's the name of this quest?</h3>
                     {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => 'A New Adventure', 'id' => 'quest_title']) }}
@@ -26,14 +26,7 @@
         </div>
     </div>
 
-    <div id="finished" style="display:none;">
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>You're all set, ready to create this quest?</h3>
 
-            </div>
-        </div>
-    </div>
 </div>
 
 <div class="col-lg-4">
@@ -50,154 +43,13 @@
     <div id="file_selection" style="display:none;">Files Attached</div>
     <div id="skills_selection">Skill #1 - 50</div>
     <div id="thresholds_selection">40 Skill #1</div>
-    {!! Form::submit('Create', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
+    {!! Form::submit('Clone', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
     {!! Form::close() !!}
 </div>
 @endsection
 
 @section('after-scripts-end')
     <script>
-    var skipThresholds = false;
-    $( "#name_next" ).click(function() {
-        $("#quest_name").hide();
-        $("#quest_name_selection").html($("#quest_title").val());
-        $("#quest_type").show();
-    });
-
-    $( "#submission_next" ).click(function() {
-        $("#quest_type").hide();
-        $("#submission_selection").show();
-    });
-
-    $( "#activity_next" ).click(function() {
-        //flag to skip minimum thresholds
-        skipThresholds = true;
-        $("#quest_type").hide();
-        $("#inclass_instant").show();
-    });
-
-    $( "#instant_allowed" ).click(function() {
-        $("#inclass_instant").hide();
-        $("#expiration").show();
-    });
-
-    $( "#instant_disallowed" ).click(function() {
-        $("#inclass_instant").hide();
-        $("#expiration").show();
-    });
-
-    $( "#watch_next" ).click(function() {
-        $("#quest_type").hide();
-        $("#video_url").show();
-    });
-
-    $( "#video_next" ).click(function() {
-        $("#video_url").hide();
-        $("#expiration").show();
-    });
-
-    $( "#link_next" ).click(function() {
-        $("#quest_type").hide();
-        $("#peer_feedback").show();
-    });
-
-    $( "#written_next" ).click(function() {
-        $("#submission_selection").hide();
-        $("#submission_revisions").show();
-    });
-
-    $( "#upload_next" ).click(function() {
-        $("#submission_selection").hide();
-        $("#submission_revisions").show();
-    });
-
-    $( "#either_next" ).click(function() {
-        $("#submission_selection").hide();
-        $("#submission_revisions").show();
-    });
-
-    $( "#revisions_allowed" ).click(function() {
-        $("#submission_revisions").hide();
-        $("#peer_feedback").show();
-    });
-
-    $( "#revisions_disallowed" ).click(function() {
-        $("#submission_revisions").hide();
-        $("#peer_feedback").show();
-    });
-
-    $( "#feedback_disallowed" ).click(function() {
-        $("#peer_feedback").hide();
-        $("#expiration").show();
-    });
-
-    $( "#feedback_allowed" ).click(function() {
-        $("#peer_feedback").hide();
-        $("#expiration").show();
-    });
-
-    $( "#expiration_allowed" ).click(function() {
-        $("#expiration").hide();
-        $("#set_expiration").show();
-    });
-
-    $( "#expiration_disallowed" ).click(function() {
-        $("#expiration").hide();
-        $("#quest_description").show();
-    });
-
-    $( "#expiration_set" ).click(function() {
-        $("#set_expiration").hide();
-        $("#quest_description").show();
-    });
-
-    $( "#description_set" ).click(function() {
-        $("#quest_description").hide();
-        $("#skills").show();
-    });
-
-    $( "#skills_set" ).click(function() {
-        $("#skills").hide();
-        if (skipThresholds) {
-            $("#files_allowed").show();
-
-        }
-        else {
-            $("#thresholds").show();
-
-        }
-    });
-
-    $( "#thresholds_allowed" ).click(function() {
-        $("#thresholds").hide();
-        $("#set_thresholds").show();
-    });
-
-    $( "#thresholds_disallowed" ).click(function() {
-        $("#thresholds").hide();
-        $("#attach_files").show();
-    });
-
-    $( "#thresholds_set" ).click(function() {
-        $("#set_thresholds").hide();
-        $("#attach_files").show();
-    });
-
-    $( "#files_allowed" ).click(function() {
-        $("#attach_files").hide();
-        $("#file_attachments").show();
-    });
-
-    $( "#set_files" ).click(function() {
-        $("#file_attachments").hide();
-        $("#finished").show();
-    });
-
-    $( "#files_disallowed" ).click(function() {
-        $("#attach_files").hide();
-        $("#finished").show();
-    });
-
 
 
 
