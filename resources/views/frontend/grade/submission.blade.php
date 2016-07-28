eeb82e8622da874e21442a422e7d6410ece3053f
Clay <c.ewing@miami.edu>
2016-07-28 09:54:34 -0400

    formatting

--- resources/views/frontend/grade/submission.blade.php
+++ resources/views/frontend/grade/submission.blade.php
@@ -19,83 +19,77 @@
 </div>              
 </div>
     <div class="col-lg-12">
-        <div class="row">
 <!--            <div><div style="width: 100%; height: 0px; position: relative; padding-bottom: 56.2493%;"><iframe src="//player.vimeo.com/video/176459945?byline=0&badge=0&portrait=0&title=0" frameborder="0" allowfullscreen style="width: 100%; height: 100%; position: absolute;"></iframe></div></div>-->
-<p>Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 
-<p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper.</p>
+        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper.</p>
 
-<p>
-Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus.
-</p>
-        </div>
-        <div class="row">
-                    <h6>Attached Files</h6>
+        <p>
+        Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus.
+        </p>
+        <h6>Attached Files</h6>
+        <a href="#" class="btn btn-default">filename.pdf</a>         
+        <a href="#" class="btn btn-default">filename.pdf</a>
+        <a href="#" class="btn btn-default">filename.pdf</a>
 
-                    <a href="#" class="btn btn-default">filename.pdf</a>         
-                    <a href="#" class="btn btn-default">filename.pdf</a>
-                    <a href="#" class="btn btn-default">filename.pdf</a>
-        </div>
+ 
+        <h4>Peer Feedback</h4>
+        <h5>Positives</h5>
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer>Someone famous in <cite title="Source Title">Joan Ridley</cite></footer>
+        </blockquote>
 
-        <div class="row">
-            <h4>Peer Feedback</h4>
-            <h5>Positives</h5>
-            <blockquote>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer>Someone famous in <cite title="Source Title">Joan Ridley</cite></footer>
-            </blockquote>
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer>Someone famous in <cite title="Source Title">Michael Scott</cite></footer>
+        </blockquote>
 
-            <blockquote>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer>Someone famous in <cite title="Source Title">Michael Scott</cite></footer>
-            </blockquote>
+        <h5>Areas for Improvement</h5>
 
-            <h5>Areas for Improvement</h5>
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer>Someone famous in <cite title="Source Title">Joan Ridley</cite></footer>
+        </blockquote>
 
-            <blockquote>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer>Someone famous in <cite title="Source Title">Joan Ridley</cite></footer>
-            </blockquote>
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer>Someone famous in <cite title="Source Title">Michael Scott</cite></footer>
+        </blockquote>
 
-            <blockquote>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer>Someone famous in <cite title="Source Title">Michael Scott</cite></footer>
-            </blockquote>
-
-        </div>
-        <div class="row">
         <h4>Feedback to Student</h4>
+
+        <div class="row">
             <div class="col-lg-9">
-            {!! Form::textarea('liked', null, ['class' => 'field', 'files' => true]) !!}
+                {!! Form::textarea('liked', null, ['class' => 'field', 'files' => true]) !!}
             </div>
             <div class="col-lg-3">
-                  <div class="form-group">
-                    <label for="skill1" class="col-sm-6 control-label">Skill #1</label>
-                    <div class="col-sm-6">
-                      <input type="number" class="form-control" id="skill1">
-                    </div>
-                  </div>
+                <div class="col-lg-6">
+                    <label>Skill #1</label>
+                </div>
+                <div class="col-lg-6">
+                    <input type="number" class="form-control" id="skill1">
+                </div>
 
-                  <div class="form-group">
-                    <label for="skill2" class="col-sm-6 control-label">Skill #2</label>
-                    <div class="col-sm-6">
-                      <input type="number" class="form-control" id="skill2">
-                    </div>
-                  </div>
+                <div class="col-lg-6">
+                    <label>Skill #2</label>
+                </div>
+                <div class="col-lg-6">
+                    <input type="number" class="form-control" id="skill2">
+                </div>
 
+                <div class="col-lg-6">
+                    <label>Skill #3</label>
+                </div>
+                <div class="col-lg-6">
+                    <input type="number" class="form-control" id="skill3">
+                </div>
 
-                  <div class="form-group">
-                    <label for="skill3" class="col-sm-6 control-label">Skill #3</label>
-                    <div class="col-sm-6">
-                      <input type="number" class="form-control" id="skill3">
-                    </div>
-                  </div>
+                <hr/>
 
-                  <hr/>
-
-                  <div class="col-lg-12">
+                <div class="col-lg-12">
                         <span>xx</span>/ 50
-                  </div>
+                </div>
 
                   <div class="col-lg-12">
                         <button type="button" class="btn btn-default btn-block">Grade</button>
