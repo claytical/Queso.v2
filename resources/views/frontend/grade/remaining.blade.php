e0f6fddb4f5f8fe6960e1c08dee785d1fff326cc
Clay <c.ewing@miami.edu>
2016-07-28 10:30:01 -0400

    adding links and formatting

--- resources/views/frontend/grade/remaining.blade.php
+++ resources/views/frontend/grade/remaining.blade.php
@@ -19,51 +19,42 @@
     <h2>Remaining Submissions for Quest</h2>
             <div id="quest-list">
             <div class="col-lg-12">
-                <div class="row">
                     <div class="col-lg-9">
                         <h4>Student Name</h4>
                     </div>
                     <div class="col-lg-3">
                         <h4>Submission Date</h4>
                     </div>
-
-                </div>                  
                   
 
                 <ul class="list-unstyled list">
                     <li>
-                        <div class="row">
                             <div class="col-lg-9 student">
-                                {{ link_to('quest/1/attempt/submission', 'Sally Fields') }}
+                                {{ link_to('grade/submission/1', 'Sally Fields') }}
                             </div>
 
                             <div class="col-lg-3 date">
                                 00/00/0000
                             </div>
-                        </div>
                     </li>
 
                     <li>
-                        <div class="row">
                             <div class="col-lg-9 student">
-                                {{ link_to('quest/1/attempt/submission', 'Don Johnson') }}
+                                {{ link_to('grade/submission/1', 'Don Johnson') }}
                             </div>
 
                             <div class="col-lg-3 date">
                                 00/00/0000
                             </div>
-                        </div>
                     </li>
                     <li>
-                        <div class="row">
                             <div class="col-lg-9 student">
-                                {{ link_to('quest/1/attempt/submission', 'Tom Hanks') }}
+                                {{ link_to('grade/submission/1', 'Tom Hanks') }}
                             </div>
 
                             <div class="col-lg-3 date">
                                 00/00/0000
                             </div>
-                        </div>
                     </li>
 
                 </ul>
