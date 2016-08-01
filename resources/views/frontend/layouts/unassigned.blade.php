c42a47285d67f3c88eda99aa744c826f67041193
Clay <c.ewing@miami.edu>
2016-08-01 18:50:04 -0400

    updating unassigned layout

--- resources/views/frontend/layouts/unassigned.blade.php
+++ resources/views/frontend/layouts/unassigned.blade.php
@@ -60,6 +60,8 @@
 
         <!-- JavaScripts -->
         {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js') }}
+        {{ HTML::script('https://cdn.tinymce.com/4/tinymce.min.js')}}
+        {{ Html::script('js/vendor/jquery/jquery.form.js') }}
 
         <script>window.jQuery || document.write('<script src="{{asset('js/vendor/jquery/jquery-2.1.4.min.js')}}"><\/script>')</script>
         {{ Html::script('js/vendor/bootstrap/bootstrap.min.js') }}
