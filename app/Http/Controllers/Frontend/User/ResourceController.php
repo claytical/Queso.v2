8701505ea927b316fa6a417cd4d2a420ceda1044
Clay <c.ewing@miami.edu>
2016-08-02 15:45:04 -0400

    create new content model

--- app/Http/Controllers/Frontend/User/ResourceController.php
+++ app/Http/Controllers/Frontend/User/ResourceController.php
@@ -50,7 +50,7 @@ class ResourceController extends Controller
     }
     
     public function save(Request $request) {
-  
+        $resource = new Content;
     //    $resource = new Content;
   //      $resource->course_id = session('current_course');
  //       $resource->title = $request->title;
