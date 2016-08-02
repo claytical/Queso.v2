da7b1069776d7d3995970e881afb98c0942c6244
Clay <c.ewing@miami.edu>
2016-08-01 21:22:35 -0400

    temp array

--- app/Http/Controllers/Frontend/User/CourseController.php
+++ app/Http/Controllers/Frontend/User/CourseController.php
@@ -103,9 +103,9 @@ class CourseController extends Controller
 
     public function add_skills() {
     	
-    	$skills = Course::find(session('current_course'))->skills();
+    	$skills = Course::find(session('current_course'))->skills;
 
-        return view('frontend.manage.course.skills', ['skills' => $skills])
+        return view('frontend.manage.course.skills', ['skills' => $skills, 'stuff' => [1,2,3]])
             ->withUser(access()->user());
     
     }
