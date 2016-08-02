0d9f4fb6f566f0f8199eb48a41c7ea068b87a59f
Clay <c.ewing@miami.edu>
2016-08-01 23:24:54 -0400

    removing errors

--- resources/views/frontend/manage/announcements/index.blade.php
+++ resources/views/frontend/manage/announcements/index.blade.php
@@ -22,22 +22,6 @@
         <div class="col-lg-12">
              <ul class="list-unstyled list">
                 @foreach($announcements as $announcement)
-                <li>
-                    <div class="row">
-                        <div class="col-lg-6 announcement">
-                            {{ link_to('manage/announcement/' . $announcement->id, $announcement->title) }}
-                        </div>
-
-                        <div class="col-lg-3 date">
-                            00/00/0000
-                        </div>
-
-                        <div class="col-lg-3">
-                            {{ link_to('manage/announcement/'.$announcement->id.'/delete', 'Delete', ['class'=> 'btn btn-danger']) }}
-                        </div>
- 
-                    </div>
-                </li>
                 @endforeach
             </ul>
     </div>
