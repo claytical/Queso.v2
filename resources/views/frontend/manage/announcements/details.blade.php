019274924f87058e5da0de044ccf7ab513da001a
Clay <c.ewing@miami.edu>
2016-08-01 23:47:25 -0400

    fill in details for announcement

--- resources/views/frontend/manage/announcements/details.blade.php
+++ resources/views/frontend/manage/announcements/details.blade.php
@@ -8,14 +8,13 @@
 <div class="col-lg-9">
     {!! Form::open(['url' => 'manage/announcement/update', 'id'=>'announcement-update-form']) !!}
     {{ Form::hidden('announcement_id', $announcement->id)}}
-    {{ Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Adventure Awaits!', 'id' => 'headline']) }}
-    {!! Form::textarea('body', null, ['class' => 'field', 'files' => true]) !!}
+    {{ Form::input('text', 'title', $announcement->title, ['class' => 'form-control', 'placeholder' => 'Adventure Awaits!', 'id' => 'headline']) }}
+    {!! Form::textarea('body', $announcement->body, ['class' => 'field', 'files' => true]) !!}
 
 </div>
 
 
 <div class="col-lg-3">
-                    {!! Form::checkbox('sticky', 1) !!} Sticky
                     {!! Form::submit('Update', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
 
                     {!! Form::close() !!}
