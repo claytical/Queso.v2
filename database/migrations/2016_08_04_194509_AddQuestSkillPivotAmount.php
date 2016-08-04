ab5b8f9391cad7afb9adca2e1cc17aa10630601b
Clay <c.ewing@miami.edu>
2016-08-04 15:45:54 -0400

    add amount to pivot table

---
+++ database/migrations/2016_08_04_194509_AddQuestSkillPivotAmount.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddQuestSkillPivotAmount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quest_skill'), function ($table) {
+            $table->integer('amount')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
