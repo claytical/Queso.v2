e1c9176b5019ccdcdecabdf1275de61c9d1330db
Clay <c.ewing@miami.edu>
2016-08-05 16:45:51 -0400

    adding quest updated

---
+++ resources/views/frontend/manage/quests/updated.blade.php
@@ -0,0 +1,18 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+<div class="col-lg-12">
+        <h2>Quest Name Updated</h2>
+
+        {{ link_to('manage/quest/create', 'Create New Quest', ['class' => 'btn btn-default btn-lg']) }}
+        {{ link_to('manage/quest/clone', 'Clone This Quest', ['class' => 'btn btn-default btn-lg']) }}
+
+</div>
+
+
+@endsection
+
+@section('after-scripts-end')
+    <script>
+    </script>
+@stop
