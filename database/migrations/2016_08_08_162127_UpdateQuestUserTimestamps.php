54c56a9d43db241f7bae924d0fdbcc63287de211
Clay <c.ewing@miami.edu>
2016-08-08 12:41:57 -0400

    removing error

--- database/migrations/2016_08_08_162127_UpdateQuestUserTimestamps.php
+++ database/migrations/2016_08_08_162127_UpdateQuestUserTimestamps.php
@@ -16,13 +16,13 @@ class UpdateQuestUserTimestamps extends Migration
 
             $table->dropForeign('quest_user_quest_id_foreign');
             $table->dropForeign('quest_user_user_id_foreign');
-            $table->integer('revision')->default(0);
+/*            $table->integer('revision')->default(0);
             $table->boolean('graded');
             $table->timestamps();
             $table->dropPrimary('quest_id');
             $table->dropPrimary('user_id');
             $table->primary(['revision', 'user_id', 'quest_id']);
-
+*/
 
         });
 
