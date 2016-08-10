c777a32429170ef1c8f6c4132f37141550402610
Clay <c.ewing@miami.edu>
2016-08-09 22:47:29 -0400

    adding part of script back

--- resources/views/frontend/quests/watch.blade.php
+++ resources/views/frontend/quests/watch.blade.php
@@ -24,4 +24,27 @@
 @endsection
 
 @section('after-scripts-end')
+    <script>
+      var tag = document.createElement('script');
+        
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          width: '100%',
+          videoId: '{!! $quest->youtube_id !!}',
+          playerVars: {'controls': 0, 'autoplay': 0},
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+    </script>
 @stop
