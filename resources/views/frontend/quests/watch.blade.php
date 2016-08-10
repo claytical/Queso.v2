7ee631dc533bbf34c72bdfb5d66af4d64cbfac9a
Clay <c.ewing@miami.edu>
2016-08-09 22:41:05 -0400

    trying old watch quest

--- resources/views/frontend/quests/watch.blade.php
+++ resources/views/frontend/quests/watch.blade.php
@@ -8,7 +8,15 @@
             </div>
             <div class="row">
                 {!! Form::open(array('url' => 'quest/watched')) !!}
-                {!! Form::submit('Get Points') !!}
+                @foreach($skills as $skill)
+                <div class="progress skill-{!! $skill->id !!}">
+                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="{!! $skill->pivot->amount !!}" style="width:0%;"></div>
+                </div>
+                {!! Form::hidden('skill[]', 0, ['id' => 'skill-'.$skill->id]) !!}
+                {!! Form::hidden('skill_id[]', $skill->id, ['id' => 'v-skill-'.$skill->id]) !!}
+
+                @endforeach
+                {!! Form::submit('Get Points', ['class' => btn btn-primary btn-submit', 'disabled' => '']) !!}
                 {!! Form::close() !!}
             </div>
         </div>
@@ -17,5 +25,62 @@
 
 @section('after-scripts-end')
     <script>
+      var tag = document.createElement('script');
+        
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          width: '100%',
+          videoId: '{{! $quest->youtube_id !}}',
+          playerVars: {'controls': 0, 'autoplay': 0},
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+//        event.target.playVideo();
+        var checkinTime = player.getDuration();
+        setInterval(checkin, checkinTime);
+
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      function checkin() {
+          var pct = (player.getCurrentTime()/player.getDuration());
+
+          @foreach($skills as $skill)
+            var amount = pct * {!! $skill->pivot->amount !!};
+            $('#skill-{!! $skill->id !!}').val(amount);
+          @endforeach
+          $('.progress-bar').attr('style', 'width: '+(pct*100)+'%');
+          if (pct >= .99) {
+            $('.btn-submit').prop('disabled', false);
+            @foreach($skills as $skill)
+                $('#skill-{!! $skill-> !!}').val({!! $skill->amount !!});
+            @endforeach
+          }
+      }
+
+      var done = false;
+
+      function onPlayerStateChange(event) {
+
+      }
+
+      function stopVideo() {
+        player.stopVideo();
+      }    
     </script>
 @stop
