4956fcdfd9ae6ee5d85c2f415c83b96d3ae9fb56
Clay <c.ewing@miami.edu>
2016-08-11 16:38:42 -0400

    update migration

--- database/migrations/2016_08_11_203059_AddCoursesToTeams.php
+++ database/migrations/2016_08_11_203059_AddCoursesToTeams.php
@@ -15,8 +15,6 @@ class AddCoursesToTeams extends Migration
         Schema::table('team_user', function ($table) {
             $table->dropForeign('team_user_team_id_foreign');
             $table->dropForeign('team_user_user_id_foreign');
-            $table->dropPrimary('user_id');
-            $table->dropPrimary('team_id');
             $table->integer('course_id')->unsigned()->index();
             $table->primary(['course_id', 'user_id', 'team_id']);
 
