5cba44e444c8a8b6b3af88d51b9aa444345cd59b
Clay <c.ewing@miami.edu>
2016-08-11 11:06:11 -0400

    start populating history

--- resources/views/frontend/quests/history.blade.php
+++ resources/views/frontend/quests/history.blade.php
@@ -5,7 +5,27 @@
 
         <div class="col-lg-12">
 
-{!! var_dump($quests) !!} 
+            @foreach($quests as $quest)
+                <div class="row">
+                    <h4>{!! $quest['quest']->name !!} </h4>
+                    <h5>Submitted {!! $quest['quest']->created_at !!}</h5>
+                    {!! $quest['quest']->instructions !!}
+
+                    @if($quest['revisions'])
+                    <h6>Revisions</h6>
+                        <ul>
+                        @foreach($quest['revisions'] as $revision)
+                            <li>{!! $revision->created_at !!}</li>
+                        @endforeach
+                        </ul>
+                    @endif
+
+
+                    <h6>00/00 Points <span class="label label-primary">Type / Revisable</span></h6>
+                    <a class="btn btn-default pull-right" href="#" role="button">View</a>
+                </div>
+            @endforeach
+ 
          </div>
 
 @endsection
