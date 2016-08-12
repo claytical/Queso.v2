89caae5d8518741e9cbff3f6bd7a15e07e2bc187
Clay <c.ewing@miami.edu>
2016-08-12 16:38:21 -0400

    changing class name

--- database/migrations/2016_08_12_201021_create_file_attachment_quest_pivot_table.php
+++ database/migrations/2016_08_12_201021_create_file_attachment_quest_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFile_attachmentQuestPivotTable extends Migration
+class CreateFileAttachmentQuestPivotTable extends Migration
 {
     /**
      * Run the migrations.
