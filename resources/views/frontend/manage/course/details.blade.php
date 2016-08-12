50c316e28cb2248f8cd03fd6b9b78232173629e8
Clay <c.ewing@miami.edu>
2016-08-12 12:13:41 -0400

    missing dash

--- resources/views/frontend/manage/course/details.blade.php
+++ resources/views/frontend/manage/course/details.blade.php
@@ -129,7 +129,7 @@
                             </div>
                         </div>
                         <div class="col-lg-1">
-                            <div class="count">{!! $team-users()->count() !!}</div>
+                            <div class="count">{!! $team->users()->count() !!}</div>
                         </div>
                         <div class="col-lg-3">
                             <div class="pull-right">
