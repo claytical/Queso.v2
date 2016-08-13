e0a773b23bcbcff632a2ec59beb0909a4eb9d2db
Clay <c.ewing@miami.edu>
2016-08-13 16:29:16 -0400

    execute query

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -53,7 +53,7 @@ class DashboardController extends Controller
             
             foreach($feedback_requests as $request) {
                 $freq = new \stdClass;
-                $freq->sender = $request->sender();
+                $freq->sender = $request->sender()->first();
                 $freq->from_user_id = $request->from_user_id;
                 $freq->revision = $request->revision;
                 $requests[] = $freq;
