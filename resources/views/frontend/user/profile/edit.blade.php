9ddb8cbfe06896aff3eb0f29209fdde2da3b0182
Clay <c.ewing@miami.edu>
2016-08-14 14:44:18 -0400

    default course id

--- resources/views/frontend/user/profile/edit.blade.php
+++ resources/views/frontend/user/profile/edit.blade.php
@@ -6,21 +6,27 @@
 
     <div class="col-lg-12">
             {{ Form::model($user, ['route' => 'frontend.user.profile.update', 'class' => 'form', 'method' => 'PATCH']) }}
-
-            {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'control-label']) }}
-                      
+            <div class="form-group">
+            {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'control-label']) }}                      
             {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
-            
+            </div>
+
             @if ($user->canChangeEmail())
-
-                {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
-                {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
-
+                <div class="form-group">
+                    {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
+                    {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
+                </div>
             @endif
-            {{ Form::label('email_notifications', "Email Notifications", ['class' => 'col-md-4 control-label']) }}
+            <div class="checkbox">
+                <label>
+                {{ Form::checkbox('email_notifications', 1) }}
+                Email Notifications</label>
+            </div>
+            <div class="form-group">
+                {{ Form::label('default_course', "Default Course", ['class' => 'control-label']) }}                      
 
-            {{ Form::checkbox('email_notifications', 1) }}
-            {{ Form::select('default_course', Form::courseList(), session('current_course', ['class' => 'selectpicker', 'id' => 'course_dropdown'])) }}
+                {{ Form::select('default_course', Form::courseList(), $user->default_course_id, ['class' => 'selectpicker', 'id' => 'course_dropdown'])) }}
+            </div>
             {{ Form::submit(trans('labels.general.buttons.save'), ['class' => 'btn btn-primary']) }}
             {{ Form::close() }}
     </div>
