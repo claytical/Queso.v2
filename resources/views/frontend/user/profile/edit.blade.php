5169cf781dc64f132c03830215ea87cf37e3f245
Clay <c.ewing@miami.edu>
2016-08-14 14:39:01 -0400

    one column

--- resources/views/frontend/user/profile/edit.blade.php
+++ resources/views/frontend/user/profile/edit.blade.php
@@ -5,10 +5,9 @@
 <h2>Settings</h2>
 
     <div class="col-lg-12">
-        <div class="col-lg-6">
-            {{ Form::model($user, ['route' => 'frontend.user.profile.update', 'class' => 'form-horizontal', 'method' => 'PATCH']) }}
+            {{ Form::model($user, ['route' => 'frontend.user.profile.update', 'class' => 'form', 'method' => 'PATCH']) }}
 
-            {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
+            {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'control-label']) }}
                       
             {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
             
@@ -18,14 +17,11 @@
                 {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
 
             @endif
-        </div>
-        <div class="col-lg-6">           
             {{ Form::label('email_notifications', "Email Notifications", ['class' => 'col-md-4 control-label']) }}
 
             {{ Form::checkbox('email_notifications', 1) }}
             {{ Form::select('default_course', Form::courseList(), session('current_course', ['class' => 'selectpicker', 'id' => 'course_dropdown'])) }}
             {{ Form::submit(trans('labels.general.buttons.save'), ['class' => 'btn btn-primary']) }}
             {{ Form::close() }}
-        </div>
     </div>
 @endsection
