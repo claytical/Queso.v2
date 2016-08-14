e9a39d23b2e199ccf59914c42f939845f71965c8
Clay <c.ewing@miami.edu>
2016-08-13 23:40:01 -0400

    flip relationship

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -9,11 +9,11 @@ class Feedback extends Model
     //
 
     public function user_from() {
-    	return $this->belongsToMany('App\Models\Access\User\User', 'from_user_id');
+    	return $this->hasOne('App\Models\Access\User\User', 'from_user_id');
     }
 
     public function user_to() {
-    	return $this->belongsToMany('App\Models\Access\User\User', 'to_user_id');
+    	return $this->hasOne('App\Models\Access\User\User', 'to_user_id');
     }
 
     public function quest() {
