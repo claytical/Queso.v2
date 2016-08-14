c15b4fbecf6c158b910cbc567ef82fccaa95e860
Clay <c.ewing@miami.edu>
2016-08-13 21:56:25 -0400

    adding back

--- resources/views/frontend/manage/student.blade.php
+++ resources/views/frontend/manage/student.blade.php
@@ -95,7 +95,24 @@
         <div class="col-lg-12">
             <div id="pending-list">
                  <ul class="list-unstyled list">
+                    @foreach($pending_quests as $quest)
+                    <li>
+                        <div class="col-lg-4 quest">
+                            {!! $quest['quest']->name !!}
+                        </div>
 
+                        <div class="col-lg-3 date">
+                            {!! $quest['quest']->created_at !!}
+                        </div>
+                        <div class="col-lg-2 revisions">
+                            {!! $quest['revisions'] !!}
+                        </div>
+                        <div class="col-lg-3 points">
+                            {!! $quest['available'] !!}
+                        </div>
+
+                    </li>
+                    @endforeach
                 </ul>
             </div>
         </div>
