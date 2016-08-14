84c322327a1167072bd3dd51affd357ecdb65fe5
Clay <c.ewing@miami.edu>
2016-08-14 13:32:50 -0400

    removal button

--- resources/views/frontend/manage/quests/details.blade.php
+++ resources/views/frontend/manage/quests/details.blade.php
@@ -69,7 +69,11 @@
         </div>
         <div role="files" class="tab-pane" id="files">
             @foreach($files as $file)
+              <div class="input-group-btn">
               {!! link_to('public/uploads/' . $file->name, $file->name, ['class' => 'btn btn-default']) !!}
+              {!! link_to('files/remove/' . $file->id, "x", ['class' => 'btn btn-danger']) !!}
+
+              </div>
             @endforeach
         </div>        
      </div>
