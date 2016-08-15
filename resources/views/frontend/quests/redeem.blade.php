cfcbbd737e8015a2a6d1a06d7ece0909326992dd
Clay <c.ewing@miami.edu>
2016-08-15 16:12:01 -0400

    qr code html

--- resources/views/frontend/quests/redeem.blade.php
+++ resources/views/frontend/quests/redeem.blade.php
@@ -5,10 +5,12 @@
 
         <div class="col-lg-12">
             <div class="row">
-                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-                {!! Form::open(array('url' => 'quest/redeem', 'class' => 'form form-inline')) !!}
+                <p id="instructions">You can hold the QR code up to your webcam to scan it, or enter the redemption code manually if you prefer.</p>
+                {!! Form::open(array('url' => 'quest/redeem', 'class' => 'form form-inline', 'id' => 'redeem-form')) !!}
         
-                {!! Form::text('code', '', ['class' => 'form-control']); !!}
+                <div id="reader" style="width:250px;height:250px">
+                 </div>
+                {!! Form::text('code', '', ['class' => 'form-control', 'id' => 'code']); !!}
                 {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
 
                 {!! Form::close() !!}
@@ -19,6 +21,23 @@
 @endsection
 
 @section('after-scripts-end')
+{{ Html::script('js/vendor/qrcode/jsqrcode-combined.min.js') }}
+{{ Html::script('js/vendor/qrcode/html5-qrcode.js') }}
+
     <script>
+$('#reader').html5_qrcode(function(data){
+        $('#code').val(data);
+        $('#reader').html5_qrcode_stop();
+        $('#redeem-form').submit();
+    },
+    function(error){
+        //show read errors 
+    }, function(videoError){
+        $('#qr_instructions').hide();
+        $('#reader').hide();
+        //the video stream could be opened
+    }
+);
+
     </script>
 @stop
