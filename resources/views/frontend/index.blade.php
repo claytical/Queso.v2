4df354fb17deaf1e052ed3571af6fd4342ee5cd7
Clay <c.ewing@miami.edu>
2016-08-15 18:25:45 -0400

    index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -15,52 +15,55 @@
     </div>
 
     <div class="col-lg-6">
-        {{ Form::open(['route' => 'auth.login', 'class' => 'form-horizontal']) }}
-            <div class="form-group">
-                {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
-                    <div class="col-md-6">
-                        {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
-                        </div><!--col-md-6-->
-            </div><!--form-group-->
+                <div class="panel panel-default">
+                    <div class="panel-heading">Login</div>
+                        <div class="panel-body">
+                        {{ Form::open(['route' => 'auth.login', 'class' => 'form-horizontal']) }}
+                            <div class="form-group">
+                                {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
+                                    <div class="col-md-6">
+                                        {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
+                                        </div><!--col-md-6-->
+                            </div><!--form-group-->
 
-            <div class="form-group">
-                {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
-                <div class="col-md-6">
-                    {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
-                </div><!--col-md-6-->
-            </div><!--form-group-->
+                            <div class="form-group">
+                                {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
+                                <div class="col-md-6">
+                                    {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
+                                </div><!--col-md-6-->
+                            </div><!--form-group-->
 
-            @if (isset($captcha) && $captcha)
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        {!! Form::captcha() !!}
-                        {{ Form::hidden('captcha_status', 'true') }}
-                    </div><!--col-md-6-->
-                </div><!--form-group-->
-            @endif
+                            @if (isset($captcha) && $captcha)
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        {!! Form::captcha() !!}
+                                        {{ Form::hidden('captcha_status', 'true') }}
+                                    </div><!--col-md-6-->
+                                </div><!--form-group-->
+                            @endif
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <div class="checkbox">
-                        <label>
-                            {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
-                        </label>
-                    </div>
-                </div><!--col-md-6-->
-            </div><!--form-group-->
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
+                                        </label>
+                                    </div>
+                                </div><!--col-md-6-->
+                            </div><!--form-group-->
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
 
-                    {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) }}
-                </div><!--col-md-6-->
-            </div><!--form-group-->
+                                    {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) }}
+                                </div><!--col-md-6-->
+                            </div><!--form-group-->
 
-            {{ Form::close() }}
-
-
-    </div>
+                            {{ Form::close() }}
+                            </div>
+                        </div>
+        </div>
 </div>
 @endsection
 
