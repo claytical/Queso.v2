068dc5c6fb54413dade6a782c668793c44285049
Clay <c.ewing@miami.edu>
2016-08-19 18:08:25 -0500

    panels

--- resources/views/frontend/quests/attempt_link.blade.php
+++ resources/views/frontend/quests/attempt_link.blade.php
@@ -4,6 +4,11 @@
         <div class="col-lg-12">
             <div class="col-lg-9">
                 <h2>{!! $quest->name !!}</h2>
+                @if($quest->expires_at)
+                    <h4>Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
+                @endif
+
+
                 {!! $quest->instructions !!}
                 {!! Form::open(array('url' => 'quest/submit', 'class' => 'form-inline')) !!}
                 {!! Form::hidden('quest_id', $quest->id) !!}       
@@ -12,42 +17,41 @@
                     {!! Form::text('link', '', ['class' => 'form-control']); !!}
                 </div>
 
-                {!! Form::submit('Submit', ['class' => 'btn btn-default']) !!}
+                {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
 
                 {!! Form::close() !!}
             </div>
             <div class="col-lg-3">
-            <ul class="list-unstyled">
-                @foreach($skills as $skill)
-                    <li>
-                        <div class="col-lg-12">
-                            <div class="col-lg-9">
-                                {!! $skill->name !!}
-                            </div>
-                            <div class="col-lg-3">
-                                {!! $skill->pivot->amount !!}
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-                    <li role="separator" class="divider"></li>
-                    <li>
-                        <div class="col-lg-12">
-                            <div class="col-lg-9">
-                            Points Total
-                            </div>
-                            <div class="col-lg-3">
-                                {!! $quest->skills()->sum('amount') !!}
-                            </div>
-                        </div>
-                    </li>
-            </ul>
-                @if($quest->expires_at)
-                <h4>Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
-                @endif
-
-
-            </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">Points Available</div>
+                  <div class="panel-body">            
+                        <ul class="list-unstyled">
+                            @foreach($skills as $skill)
+                                <li>
+                                    <div class="col-lg-12">
+                                        <div class="col-lg-9">
+                                            {!! $skill->name !!}
+                                        </div>
+                                        <div class="col-lg-3">
+                                            {!! $skill->pivot->amount !!}
+                                        </div>
+                                    </div>
+                                </li>
+                            @endforeach
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <div class="col-lg-12">
+                                        <div class="col-lg-9">
+                                        Points Total
+                                        </div>
+                                        <div class="col-lg-3">
+                                            {!! $quest->skills()->sum('amount') !!}
+                                        </div>
+                                    </div>
+                                </li>
+                        </ul>
+                    </div>
+                </div>
             
         </div>
 
