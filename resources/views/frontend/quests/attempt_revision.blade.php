1c47a80e5bf7afd0bd5242faa9c0c67cffd16f12
Clay <c.ewing@miami.edu>
2016-08-19 18:58:31 -0500

    reconfig resubmission

--- resources/views/frontend/quests/attempt_revision.blade.php
+++ resources/views/frontend/quests/attempt_revision.blade.php
@@ -1,82 +1,94 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-        <div class="col-lg-12">
+
+{!! Form::open(array('url' => 'quest/submit', 'class' => 'form-inline')) !!}
+
+<div class="col-lg-12">
             <div class="col-lg-9">
-                <h2>{!! $quest->name !!}</h2>
-                {!! $quest->instructions !!}
-                {!! Form::open(array('url' => 'quest/submit', 'class' => 'form-inline')) !!}
-                {!! Form::hidden('revision', $previous_attempt->revision + 1) !!}
-                {!! Form::hidden('quest_id', $quest->id) !!}
-                    @if($quest->quest_type_id == 1)
-
-                        <div class="form-group">
-                            @if($quest->submissions)
-                                {!! Form::textarea('submission', $previous_attempt->submission); !!}
-                            @endif
-                        @if($quest->uploads)                        
-                            <div id="submission_upload">Drop Files Here</div>
-                        @endif
-                        </div>
-                    @endif
-                    @if($quest->quest_type_id == 4)
-                        <div class="form-group">
-                        {!! Form::text('link', null); !!}
-                        </div>
-                    @endif
-
-                {!! Form::submit('Submit Revision', ['class' => 'btn btn-default']) !!}
-
-                {!! Form::close() !!}
+                <div class="col-lg-12">
+                    <h2>{!! $quest->name !!}</h2>
+                        {!! $quest->instructions !!}
+                        {!! Form::hidden('revision', $previous_attempt->revision + 1) !!}
+                        {!! Form::hidden('quest_id', $quest->id) !!}
+                </div>
             </div>
             <div class="col-lg-3">
-            @if(!empty($existing_skills[0]))
+            </div> 
+<div class="col-lg-12">
+    <div class="col-lg-9">
+            @if($quest->quest_type_id == 1)           
+                @if($quest->submissions)
+                    {!! Form::textarea('submission', $previous_attempt->submission); !!}
+                @endif
+            @endif
 
-                <ul class="list-unstyled">
-                    @foreach($skills as $index => $skill)
-                        <li>
+            @if($quest->quest_type_id == 4)
+                <div class="form-group">
+                {!! Form::text('link', null); !!}
+                </div>
+            @endif
+    </div>
+
+    <div class="col-lg-3">
+
+        @if($quest->uploads)                        
+            <div id="submission_upload" class="dropzone"></div>
+        @endif
+
+        @if(!empty($existing_skills[0]))
+           <ul class="list-unstyled">
+                @foreach($skills as $index => $skill)
+                    <li>
+                        <div class="col-lg-12">
                             <div class="col-lg-12">
-                                <div class="col-lg-12">
-                                    <h4>Current Grade</h4>
-                                </div>
-                                <div class="col-lg-6">
-                                    {!! $skill->name !!}
-                                </div>
-                                <div class="col-lg-6">                                
-                                    {!! $existing_skills[$index]->pivot->amount !!} / {!! $skill->pivot->amount !!}
-                                </div>
+                                <h4>Current Grade</h4>
                             </div>
-                        </li>
-                    @endforeach
-                        <li role="separator" class="divider"></li>
-                        <li>
-                            <div class="col-lg-12">
-                                <div class="col-lg-6">
-                                Points Total
-                                </div>
-                                <div class="col-lg-6">
-                                   {!! $total !!} / {!! $quest->skills()->sum('amount') !!}
-                                </div>
+                            <div class="col-lg-6">
+                                {!! $skill->name !!}
                             </div>
-                        </li>
-                </ul>
+                            <div class="col-lg-6">                                
+                                {!! $existing_skills[$index]->pivot->amount !!} / {!! $skill->pivot->amount !!}
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
+                    <li role="separator" class="divider"></li>
+                    <li>
+                        <div class="col-lg-12">
+                            <div class="col-lg-6">
+                            Points Total
+                            </div>
+                            <div class="col-lg-6">
+                               {!! $total !!} / {!! $quest->skills()->sum('amount') !!}
+                            </div>
+                        </div>
+                    </li>
+            </ul>
             @else
                 <h3><span class="label label-danger">UNGRADED</span></h3>
                 <p>By submitting this revision, your previously submitted and ungraded attempt will be discarded.</p>
             @endif
-                @if($quest->expires_at)
-                    <h4>Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
-                @endif
 
-                  @if(!$files->isEmpty())
-                    <h4>Previously Submitted Files</h4>
-                    @foreach($files as $file)
-                        {!! link_to('public/uploads/' . $file->name, $file->name, ['class' => 'btn btn-default']) !!}
-                    @endforeach
-                @endif
-            </div>
+            @if($quest->expires_at)
+                <h4>Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
+            @endif
+
+              @if(!$files->isEmpty())
+                <h4>Previously Submitted Files</h4>
+                @foreach($files as $file)
+                    {!! link_to('public/uploads/' . $file->name, $file->name, ['class' => 'btn btn-default']) !!}
+                @endforeach
+                
+            @endif
             
-        </div>
+            {!! Form::submit('Submit Revision', ['class' => 'btn btn-primary btn-block']) !!}
+
+    </div>
+            
+</div>
+
+{!! Form::close() !!}
 
 @endsection
 
