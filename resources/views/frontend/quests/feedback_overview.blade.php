30b0b33cef81661ab33051e617c030f667d492a7
Clay <c.ewing@miami.edu>
2016-08-19 18:01:36 -0500

    swap condition

--- resources/views/frontend/quests/feedback_overview.blade.php
+++ resources/views/frontend/quests/feedback_overview.blade.php
@@ -3,10 +3,7 @@
 @section('content')
             <div class="col-lg-6">
                 <h2>Feedback Received</h2>
-                    @if($feedback_received->isEmpty())
-                        <p>You haven't received any feedback...</p>
-                    @else
-
+                    @if($feedback_received)
                         @foreach($feedback_received as $received)
                             @if($received->fulfilled > 0)
                                 <h4>{{ link_to('quest/'.$received->quest_id.'/feedback', $received->quest_name) }}</h4>
@@ -29,6 +26,8 @@
                                     </ul>
                             @endif
                         @endforeach
+                    @else
+                        <p>You haven't received any feedback...</p>                        
                     @endif
             </div>
 
