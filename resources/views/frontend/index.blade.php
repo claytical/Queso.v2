4260326c96e869d9090f6f09df6fa51e60a021b7
Clay <c.ewing@miami.edu>
2016-08-20 17:06:25 -0500

    reformat index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -65,11 +65,11 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
+                                    {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary btn-block']) }}
 
-                                    {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) }}
                                 </div><!--col-md-6-->
                             </div><!--form-group-->
+                                    {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password', ['class' => 'pull-right'])) }}
 
                             {{ Form::close() }}
                             </div>
