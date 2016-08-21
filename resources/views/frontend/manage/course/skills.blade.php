b81bec5085e35a718d71a3cde494db5de2067bcb
Clay <c.ewing@miami.edu>
2016-08-21 18:28:46 -0400

    formatting skill list

--- resources/views/frontend/manage/course/skills.blade.php
+++ resources/views/frontend/manage/course/skills.blade.php
@@ -10,10 +10,10 @@
         <div id="skills">
             @foreach($skills as $skill)
                 <div class="row">
-                    {!! $skill->name !!}
                     {!! Form::open(['url' => 'course/remove/skill', 'class' => 'remove-skill']) !!}
                     {!! Form::hidden('skill', $skill->id) !!}
                     {!! Form::submit('Remove', ['class' => 'btn btn-danger btn-xs pull-right']) !!}                           
+                    <h5>{!! $skill->name !!}</h5>
                     {!! Form::close() !!}
                 </div>
             @endforeach
