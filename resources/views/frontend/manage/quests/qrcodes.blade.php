33a581278a25fda26b94f9a84a95dbc2c6a19f5d
Clay <c.ewing@miami.edu>
2016-08-21 15:36:20 -0400

    formatting qr code generation page

--- resources/views/frontend/manage/quests/qrcodes.blade.php
+++ resources/views/frontend/manage/quests/qrcodes.blade.php
@@ -3,38 +3,42 @@
 @section('content')
 <div class="col-lg-12">
     <h2>{!! $quest->name !!} Redemption Codes</h2>
-	{{ link_to('manage/quest/'.$quest->id.'/qrcards', 'Print Cards', ['class' =>'btn btn-default btn-xs pull-right']) }}    
+	{{ link_to('manage/quest/'.$quest->id.'/qrcards', 'Print Cards', ['class' =>'btn btn-default btn-lg pull-right', '_target' => 'blank']) }}    
 </div>
-<div class="col-lg-12">
-	 {!! Form::open(array('url' => 'manage/quest/qrcards')) !!}
-	 {!! Form::hidden('quest_id', $quest->id) !!}
-	 		<div class="form-group">
-      			<label>Number of Codes to Generate</label>
-            {{ Form::input('number', 'new_codes', null, ['class' => 'form-control', 'placeholder' => '0', 'id' => 'redemption_codes']) }}
-                       <span class="input-group-btn">
-					      {!! Form::submit('Generate', ['class' => 'btn btn-primary']) !!}
-                        </span>            
-            </div>
-</div>
-<h3>Unused Codes</h3>
 <div class="col-md-12">
+@if(!$unused_codes->isEmpty())	
+	<h3>Unused Codes</h3>
 	@foreach($unused_codes as $code)
 	  <div class="col-md-3">
 	  <h5>{!! $code->code !!}</h5>
 	  </div>
 	@endforeach
+@endif
 </div>
 
-<h3>Used Codes</h3>
 <div class="col-md-12">
+@if(!$unused_codes->isEmpty())
+	<h3>Used Codes</h3>
 	@foreach($used_codes as $code)
 	  <div class="col-md-3">
 	  <h5>{!! $code->code !!}</h5>
 	  <h6>{!! $code->user()->name !!}</h6>
 	  </div>
 	@endforeach
+@endif
 </div>
  
+<div class="col-lg-12">
+	 {!! Form::open(array('url' => 'manage/quest/qrcards')) !!}
+	 {!! Form::hidden('quest_id', $quest->id) !!}
+<h4>Generate Codes</h4>
+	<div class="input-group">
+    {{ Form::input('number', 'new_codes', null, ['class' => 'form-control', 'placeholder' => '0', 'id' => 'redemption_codes']) }}
+               <span class="input-group-btn">
+			      {!! Form::submit('Generate', ['class' => 'btn btn-primary']) !!}
+                </span>            
+    </div>
+</div>
 
 @endsection
 
