43e81cf9d3f863584eed47521b3f3e269bcfd000
Clay <c.ewing@miami.edu>
2016-08-23 11:59:25 -0400

    quest management table

--- resources/views/frontend/manage/quests/index.blade.php
+++ resources/views/frontend/manage/quests/index.blade.php
@@ -8,49 +8,35 @@
 </div>
 
 @if(!$quests->isEmpty())
-<div class="row">
-        <div class="col-lg-9">
-            <h5>Quest Name</h5>
-        </div>
-        <div class="col-lg-3">
-            <h5></h5>
-        </div>
-        <div class="col-lg-12">
-             <ul class="list-unstyled list">
-
+    <table class="table table-hover">
+            <thead>
+                <th>Quest</th>
+                <th></th>
+                <th></th>
+            </thead>
                 @foreach($quests as $quest)
-                <li>
-                    <div class="row extra-top-padding">
-                        <div class="col-lg-6 quest">
-                            {{ link_to('manage/quest/'.$quest->id, $quest->name) }}
-                        </div>
-
-                        <div class="col-lg-3 category">
-                            
-                        </div>
-                        <div class="col-lg-3">
-
-                            <div class="btn-group">
-                              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Action <span class="caret"></span>
-                              </button>
-                              <ul class="dropdown-menu">
-                              <li>{{ link_to('manage/quest/'.$quest->id, 'Edit') }}</li>
-                                <li>{{ link_to('quest/'.$quest->id.'/attempt/submission', 'View as Student')}}</li>
-                              @if($quest->instant)
-                                <li><a href="{!! url('manage/quest/'.$quest->id.'/qrcards');!!}" target="_blank"><span class="glyphicon glyphicon-qrcode"></span> QR Code Sheet</a></li>
-                              @endif
-                                <li><a href="{!! url('manage/quest/'.$quest->id.'/clone');!!}"><span class="glyphicon glyphicon-copy"></span> Clone</a></li>
-                                <li><a href="{!! url('manage/quest/'.$quest->id.'/delete');!!}"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>
-                              </ul>
-                            </div>
-                        </div>                         
-                    </div>
-                </li>
+                    <tr>
+                        <td>{{ link_to('manage/quest/'.$quest->id, $quest->name) }}</td>
+                        <td></td>
+                        <td>
+                                <div class="btn-group">
+                                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Action <span class="caret"></span>
+                                  </button>
+                                  <ul class="dropdown-menu">
+                                  <li>{{ link_to('manage/quest/'.$quest->id, 'Edit') }}</li>
+                                    <li>{{ link_to('quest/'.$quest->id.'/attempt/submission', 'View as Student')}}</li>
+                                  @if($quest->instant)
+                                    <li><a href="{!! url('manage/quest/'.$quest->id.'/qrcards');!!}" target="_blank"><span class="glyphicon glyphicon-qrcode"></span> QR Code Sheet</a></li>
+                                  @endif
+                                    <li><a href="{!! url('manage/quest/'.$quest->id.'/clone');!!}"><span class="glyphicon glyphicon-copy"></span> Clone</a></li>
+                                    <li><a href="{!! url('manage/quest/'.$quest->id.'/delete');!!}"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>
+                                  </ul>
+                                </div>
+                        </td>
+                    </tr>
                 @endforeach
-            </ul>
-    </div>
-</div>
+    </table>
 @else
 <p class="lead">There are currently no quests.</p>
 @endif
