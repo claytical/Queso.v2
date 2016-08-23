162f40152118d46b35c2152800f24997496c640f
Clay <c.ewing@miami.edu>
2016-08-23 12:02:00 -0400

    quest management button

--- resources/views/frontend/manage/quests/index.blade.php
+++ resources/views/frontend/manage/quests/index.blade.php
@@ -19,6 +19,7 @@
                         <td>{{ link_to('manage/quest/'.$quest->id, $quest->name) }}</td>
                         <td></td>
                         <td>
+                            <div class="pull-right">
                                 <div class="btn-group">
                                   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Action <span class="caret"></span>
@@ -33,6 +34,7 @@
                                     <li><a href="{!! url('manage/quest/'.$quest->id.'/delete');!!}"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>
                                   </ul>
                                 </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
