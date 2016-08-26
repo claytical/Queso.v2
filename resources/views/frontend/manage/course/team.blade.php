67958e29f2841448995adf1dfab8e9678b821409
Clay <c.ewing@miami.edu>
2016-08-25 20:03:39 -0400

    team select

--- resources/views/frontend/manage/course/team.blade.php
+++ resources/views/frontend/manage/course/team.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="col-xs-5">
 
-                <select name="from[]" class="multiselect form-control" size="8" multiple="multiple" data-right="#multiselect_to_1" data-right-all="#right_All_1" data-right-selected="#right_Selected_1" data-left-all="#left_All_1" data-left-selected="#left_Selected_1">
+                <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
                 @foreach($students_not_on_team as $student)
 
                     <option value="{!! $student->id !!}">{!! $student->name !!}</option>
@@ -15,16 +15,17 @@
 
             <div class="col-xs-2">
                 &nbsp;
-                <button type="button" id="right_All_1" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
-                <button type="button" id="right_Selected_1" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
-                <button type="button" id="left_Selected_1" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
-                <button type="button" id="left_All_1" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
+                <button type="button" id="right_All" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
+                <button type="button" id="right_Selected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
+                <button type="button" id="left_Selected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
+                <button type="button" id="left_All" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
 
             </div>
 
             <div class="col-xs-5">
-                <select name="to[]" class="multiselect form-control" size="8" multiple="multiple" data-right="#multiselect_to_1" data-right-all="#right_All_1" data-right-selected="#right_Selected_1" data-left-all="#left_All_1" data-left-selected="#left_Selected_1">
+                <select name="to[]" class="form-control" size="8" multiple="multiple">
                 @foreach($students as $student)
+                    <option value="{!! $student->id !!}">{!! $student->name !!}</option>
                     @endforeach
                 </select>
             </div>
@@ -35,7 +36,7 @@
     <script>
 
     jQuery(document).ready(function($) {
-        $('.multiselect').multiselect();
+        $('#multiselect').multiselect();
     });
 
 
