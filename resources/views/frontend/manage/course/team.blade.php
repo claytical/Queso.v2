bfd28e7aee923a5b2178fe634c64595afd1ff52e
Clay <c.ewing@miami.edu>
2016-08-25 20:11:00 -0400

    default multiselect

--- resources/views/frontend/manage/course/team.blade.php
+++ resources/views/frontend/manage/course/team.blade.php
@@ -2,34 +2,28 @@
 
 @section('content')
     <h2>{!! $team->name !!}</h2>
-        <div class="col-lg-12">
-            <div class="col-xs-5">
-
-                <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
-                @foreach($students_not_on_team as $student)
-
-                    <option value="{!! $student->id !!}">{!! $student->name !!}</option>
-                @endforeach
-                </select>
-            </div>
-
-            <div class="col-xs-2">
-                &nbsp;
-                <button type="button" id="right_All" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
-                <button type="button" id="right_Selected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
-                <button type="button" id="left_Selected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
-                <button type="button" id="left_All" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
-
-            </div>
-
-            <div class="col-xs-5">
-                <select name="to[]" class="form-control" size="8" multiple="multiple">
-                @foreach($students as $student)
-                    <option value="{!! $student->id !!}">{!! $student->name !!}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
+<div class="row">
+    <div class="col-xs-5">
+        <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
+            <option value="1">Item 1</option>
+            <option value="2">Item 5</option>
+            <option value="2">Item 2</option>
+            <option value="2">Item 4</option>
+            <option value="3">Item 3</option>
+        </select>
+    </div>
+    
+    <div class="col-xs-2">
+        <button type="button" id="multiselect_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
+        <button type="button" id="multiselect_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
+        <button type="button" id="multiselect_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
+        <button type="button" id="multiselect_leftAll" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
+    </div>
+    
+    <div class="col-xs-5">
+        <select name="to[]" id="multiselect_to" class="form-control" size="8" multiple="multiple"></select>
+    </div>
+</div>
 @endsection
 
 @section('after-scripts-end')
