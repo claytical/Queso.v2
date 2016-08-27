b9b7334c9f96085e435bb6e4b99b0cf3309018a7
Clay <c.ewing@miami.edu>
2016-08-27 12:39:35 -0400

    alignment and header

--- resources/views/frontend/manage/course/team.blade.php
+++ resources/views/frontend/manage/course/team.blade.php
@@ -6,6 +6,7 @@
 {!! Form::hidden('team_id', $team->id ) !!}
 
     <div class="col-xs-5">
+        <b>Not on Team</b>
         <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
         @foreach($students_not_on_team as $student)
             <option value="{!! $student->id !!}">{!! $student->name !!}</option>
@@ -14,6 +15,7 @@
     </div>
     
     <div class="col-xs-2">
+    &nbsp;
         <button type="button" id="multiselect_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
         <button type="button" id="multiselect_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
         <button type="button" id="multiselect_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
@@ -21,6 +23,7 @@
     </div>
     
     <div class="col-xs-5">
+        <b>On Team</b>
         <select name="to[]" id="multiselect_to" class="form-control" size="8" multiple="multiple">
             @foreach($students as $student)
                 <option value="{!! $student->id !!}">{!! $student->name !!}</option>
