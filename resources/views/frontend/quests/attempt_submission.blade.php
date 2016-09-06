097cc214443cf42d524e034ff9b5c2298f329f64
Clay Ewing <c.ewing@miami.edu>
2016-09-06 15:34:41 -0400

    remove div

--- resources/views/frontend/quests/attempt_submission.blade.php
+++ resources/views/frontend/quests/attempt_submission.blade.php
@@ -39,9 +39,7 @@
 
                 @if($quest->groups)
                     <h5>Group Members</h5>
-                    <div class="col-lg-12">
                         {!! Form::studentList('students', null, ['multiple' => 'multiple', 'class' => 'multiselect']) !!}
-                    </div>
                     <hr/>
                 @endif
 
