02afa03bd1818acee0505d314dd6493cd54d99f8
Clay <c.ewing@miami.edu>
2016-09-10 13:41:15 -0400

    zero out points earned

--- app/Http/Controllers/Frontend/User/StudentController.php
+++ app/Http/Controllers/Frontend/User/StudentController.php
@@ -68,6 +68,9 @@ class StudentController extends Controller
         }
 
         $total_points_earned = $user->skills()->where('course_id', '=', session('current_course'))->sum('amount');
+        if(!$total_points_earned) {
+            $total_points_earned = 0;
+        }
         $current_level = $course->levels()->where('amount', '<=', $total_points_earned)->orderBy('amount', 'desc')->first();
         $next_level = $course->levels()->where('amount', '>', $total_points_earned)->orderBy('amount', 'desc')->first();
 
