238b7adaae616e2dabb45afe6e5429e867295c52
Clay <c.ewing@miami.edu>
2016-09-12 15:20:26 -0400

    test

--- resources/views/frontend/manage/course/details.blade.php
+++ resources/views/frontend/manage/course/details.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
     <div class="col-lg-12">
-        <a class="btn btn-default btn-xs pull-right" href="mailto:{!! Form::courseEmailList() !!}">Mail Entire Class</a>
         <h2>Manage {!! $course->name !}}</h2>
 
     </div>
