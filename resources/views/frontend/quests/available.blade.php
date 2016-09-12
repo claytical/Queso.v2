3033e08fa04ed9d5a28284bc85d804624e1c3016
Clay <c.ewing@miami.edu>
2016-09-12 14:42:51 -0400

    add modals

--- resources/views/frontend/quests/available.blade.php
+++ resources/views/frontend/quests/available.blade.php
@@ -20,8 +20,9 @@
                 @foreach($unlocked as $quest)
                     <tr>
                         <td>
+                        data-toggle="modal" data-target="#myModal"
                             @if($quest->quest_type_id == 1)
-                                <h4>{{ link_to('quest/'.$quest->id.'/attempt/submission', $quest->name) }}</h4>
+                                <h4>{{ link_to('quest/'.$quest->id.'/attempt/submission', $quest->name, ['data-toggle' => 'modal', 'data-target' => '#quest-' . $quest->id]) }}</h4>
                             @endif
                             @if($quest->quest_type_id == 2)
                                 <h4>{{ $quest->name }}</h4>
@@ -48,6 +49,35 @@
                 @endforeach
             </tbody>
         </table>
+    @foreach($unlocked as $quest)
+
+        <div class="modal fade" tabindex="-1" role="dialog" id="quest-{!! $quest->id !!}">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">{!! $quest->name !!}</h4>
+              </div>
+              <div class="modal-body">
+                <p>{!! $quest->instructions !!}</p>
+                    
+                    @if($quest->quest_type_id == 1)
+                        {{ link_to('quest/'.$quest->id.'/attempt/submission', 'Attempt', ['class' => 'btn btn-primary']) }}
+                    @endif
+
+                    @if($quest->quest_type_id == 3)
+                        {{ link_to('quest/'.$quest->id.'/watch', 'Watch', ['class' => 'btn btn-primary']) }}
+                    @endif
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->    
+    @endforeach
+
 @else
     <p>There are no available quests.</p>
 @endif
