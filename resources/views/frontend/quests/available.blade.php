bbcc47325faa8e68a1846f078108f4dc09c13055
Clay <c.ewing@miami.edu>
2016-09-12 14:56:55 -0400

    revisable table

--- resources/views/frontend/quests/available.blade.php
+++ resources/views/frontend/quests/available.blade.php
@@ -68,6 +68,67 @@
     <p>There are no available quests.</p>
 @endif
 
+@if(!$revisable->isEmpty())
+<h2>Quests That Can Be Revised for More Points</h2>
+    <table class="table table-hover" data-toggle="table" data-classes="table-no-bordered">
+        <thead>
+            <tr>
+                <th data-field="name" 
+                data-sortable="true">Quest</th>
+                <th data-field="points" 
+                data-sortable="true">Points</th>
+                <th data-field="expiration" 
+                data-sortable="true">Expires</th>
+            </tr>            
+        </thead>
+            <tbody>
+        @foreach($revisable as $quest)
+                    <tr>
+                        <td>
+                            {{ link_to('#', $quest->name, ['data-toggle' => 'modal', 'data-target' => '#quest-' . $quest->id]) }}
+                        </td>
+                        <td>
+                            {!! $quest->skills()->sum('amount') !!}
+                        </td>
+                        <td>
+                            @if($quest->expires_at)
+                            {!! date('m-d-Y', strtotime($quest->expires_at)) !!}
+                            @else
+                            Never
+                            @endif
+                        </td>
+                    </tr>
+        @endforeach
+                </tbody>
+            </table>
+
+    @foreach($revisable as $quest)
+
+        <div class="modal fade" tabindex="-1" role="dialog" id="quest-{!! $quest->id !!}">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">{!! $quest->name !!}</h4>
+              </div>
+              <div class="modal-body">
+                <p>{!! $quest->instructions !!}</p>
+                    
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    {{ link_to('quest/'.$quest->id.'/revise', 'Revise', ['class' => 'btn btn-primary']) }}
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->    
+    @endforeach
+
+
+
+
+@endif
+
 @if($locked)
 <h2>Quests Requiring Higher Skill Levels</h2>
         <div class="col-lg-12">
@@ -87,27 +148,6 @@
         </div>
 @endif
 
-@if(!$revisable->isEmpty())
-<h2>Quests That Can Be Revised for More Points</h2>
-    
-    <div class="col-lg-12">
-        @foreach($revisable as $quest)
-
-            <div class="row">
-                <div class="col-lg-9">
-                    <h4>{{ link_to('quest/'.$quest->id.'/revise', $quest->name) }}</h4>
-                    <h5>{!! $quest->skills()->sum('amount') !!} Points</h5>
-                    @if($quest->expires_at)
-                    <h6>Due by {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h6>
-                    @endif
-                </div>
-                <div class="col-lg-12">
-                    {!! $quest->instructions !!}
-                </div>
-            </div>
-        @endforeach
-    </div>
-@endif
 @endsection
 
 @section('after-scripts-end')
