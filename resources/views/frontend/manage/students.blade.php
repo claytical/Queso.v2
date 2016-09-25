fa6f0a7e4fd264743ee414dd29d1672f6d14108d
Clay <c.ewing@miami.edu>
2016-09-25 16:07:50 -0400

    toggle points

--- resources/views/frontend/manage/students.blade.php
+++ resources/views/frontend/manage/students.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h2>Manage Students</h2>
-
+<a href="#" class="btn btn-default pull-right" id="show_points">Show Points</a>
 @if(!$students->isEmpty())
 <table class="table table-hover" data-toggle="table" data-classes="table-no-bordered">
         <thead>
@@ -19,7 +19,7 @@
             <tr>
                 <td>{{ link_to('manage/student/'.$student->id, $student->name) }}</td>
                 <td><a href="mailto:{!! $student->email !!}">{!! $student->email !!}</td>
-                <td>{!! $student->skills()->where('course_id', '=', session('current_course'))->sum('amount') !!}</td>
+                <td style="display: none;" class="grades">{!! $student->skills()->where('course_id', '=', session('current_course'))->sum('amount') !!}</td>
                 <td>{{ link_to('manage/student/'.$student->id.'/leave', 'Remove') }}</td>
             </tr>
         @endforeach
@@ -33,6 +33,17 @@
 
 @section('after-scripts-end')
     <script>
+    var showing_grades = false;
+    $('#show_points').click(function() {
+        $('.grades').toggle();
+        showing_grades = !showing_grades;
+        if(showing_grades) {
+            $(this).text("Hide Points");
+        }
+        else {
+            $(this).text("Show Points");
+        }
+    });
   /*      var quest_list_options = {
         valueNames: [ 'submission', 'date', 'student' ]
     };
