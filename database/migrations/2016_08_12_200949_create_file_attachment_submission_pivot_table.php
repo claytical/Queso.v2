e3a85031aa7eae979aecdb0799980e201f89079f
Clay Ewing <c.ewing@miami.edu>
2017-07-10 18:36:19 -0400

    update migration

--- database/migrations/2016_08_12_200949_create_file_attachment_submission_pivot_table.php
+++ database/migrations/2016_08_12_200949_create_file_attachment_submission_pivot_table.php
@@ -12,18 +12,18 @@ class CreateFileAttachmentSubmissionPivotTable extends Migration
      */
     public function up()
     {
-        Schema::table('file_attachment_submission', function ($table) {
-            $table->primary(['file_attachment_id', 'submission_id'], 'file_sub_primary');
-
-        });
-/*        Schema::create('file_attachment_submission', function (Blueprint $table) {
+        Schema::create('file_attachment_submission', function (Blueprint $table) {
             $table->integer('file_attachment_id')->unsigned()->index();
             $table->foreign('file_attachment_id')->references('id')->on('file_attachments')->onDelete('cascade');
             $table->integer('submission_id')->unsigned()->index();
             $table->foreign('submission_id')->references('id')->on('submissions')->onDelete('cascade');
             $table->primary(['file_attachment_id', 'submission_id'], 'file_sub_primary');
         });
-        */
+
+        Schema::table('file_attachment_submission', function ($table) {
+            $table->primary(['file_attachment_id', 'submission_id'], 'file_sub_primary');
+
+        });
     }
 
     /**
