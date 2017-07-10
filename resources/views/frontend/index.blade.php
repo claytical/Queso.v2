874ef20d373cb7d22cccfa196c89a7deb7b2929c
Clay Ewing <c.ewing@miami.edu>
2017-07-10 19:53:01 -0400

    update landing

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,8 +6,9 @@
 
 
 @section('content')
-<img src="{!! URL::to('img/logo.png')!!}" alt="Queso: A Gameful Learning Management System"/>
-
+<div class="container is-fluid">
+    <img src="{!! URL::to('img/logo.png')!!}" alt="Queso: A Gameful Learning Management System"/>
+</div>
 <div class="tile">
   <div class="tile is-6 is-parent">
     <div class="tile is-child box">
@@ -21,26 +22,32 @@
         {{ Form::open(['route' => 'auth.login', 'class' => '']) }}
         
         <div class="field">
-            {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'label']) }}        
-          <p class="control">
-            {{ Form::input('email', 'email', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.email')]) }}          
+          {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'label']) }}
+          <p class="control has-icons-left has-icons-right">
+            {{ Form::input('email', 'email', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
             <span class="icon is-small is-left">
               <i class="fa fa-envelope"></i>
             </span>
             <span class="icon is-small is-right">
-              <i class="fa fa-check"></i>
+              <i class="fa fa-warning"></i>
             </span>
           </p>
+          <p class="help is-danger">This email is invalid</p>
         </div>
 
+
         <div class="field">
-            {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'label']) }}        
-          <p class="control">
-            {{ Form::input('password', 'password', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
+          {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'label']) }}
+          <p class="control has-icons-left has-icons-right">
+            {{ Form::input('password', 'password', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.password')]) }}          
             <span class="icon is-small is-left">
               <i class="fa fa-lock"></i>
             </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-warning"></i>
+            </span>
           </p>
+          <p class="help is-danger">This email is invalid</p>
         </div>
 
         <div class="field is-horizontal">
@@ -58,7 +65,7 @@
         {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'button is-primary']) }}
         {{ Form::close() }}
 
-        {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password', ['class' => 'button is-link'])) }}
+        {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password', ['class' => 'button is-link is-pulled-right'])) }}
     </div>
   </div>
 </div>
