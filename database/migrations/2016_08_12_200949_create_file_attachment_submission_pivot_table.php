566bf7a4a98be348bca1c0618a4d2b9a9a357409
Clay Ewing <c.ewing@miami.edu>
2017-07-10 18:38:36 -0400

    move file attachment migration back

--- database/migrations/2016_08_12_200949_create_file_attachment_submission_pivot_table.php
+++ database/migrations/2016_08_12_200949_create_file_attachment_submission_pivot_table.php
@@ -12,7 +12,7 @@ class CreateFileAttachmentSubmissionPivotTable extends Migration
      */
     public function up()
     {
-  /*
+  
         Schema::create('file_attachment_submission', function (Blueprint $table) {
             $table->integer('file_attachment_id')->unsigned()->index();
             $table->foreign('file_attachment_id')->references('id')->on('file_attachments')->onDelete('cascade');
@@ -20,7 +20,7 @@ class CreateFileAttachmentSubmissionPivotTable extends Migration
             $table->foreign('submission_id')->references('id')->on('submissions')->onDelete('cascade');
             $table->primary(['file_attachment_id', 'submission_id'], 'file_sub_primary');
         });
-*/
+
     }
 
     /**
