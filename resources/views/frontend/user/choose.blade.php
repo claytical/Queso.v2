a6d737cab55e67243ba58974f5be44f493f96e94
Clay Ewing <c.ewing@miami.edu>
2017-07-10 21:04:18 -0400

    update choose

--- resources/views/frontend/user/choose.blade.php
+++ resources/views/frontend/user/choose.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 
-<section class="hero is-info is-large">
+<section class="hero is-dark is-bold is-large">
   <div class="hero-body">
     <div class="container is-fluid">
       <h1 class="title">
         Choose Your Destiny
       </h1>
-      <h2 class="subtitle"></h2>
-		<p>Before we begin, are you a student looking to add a class or an instructor looking to create one?</p>      
+      <h2 class="subtitle">Before we begin, are you a student looking to add a class or an instructor looking to create one?</h2>
     
 		<a class="button is-large" href="{!! URL::to('course/join') !!}">Student</a>
 
