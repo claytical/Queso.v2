517302588877320c5e67007c1c8b345f576782ad
Clay Ewing <c.ewing@miami.edu>
2017-07-11 14:29:31 -0400

    update manage levels

--- resources/views/frontend/manage/course/levels.blade.php
+++ resources/views/frontend/manage/course/levels.blade.php
@@ -10,7 +10,7 @@
         Levels
       </h1>
       <h2 class="subtitle">You probably want more than one level. Traditionally, most classes require over 60% to get a D. That means most of your students will have an F for the majority of the class and make great progress towards the end of the course. To encourage motivation, try creating levels in between levels that correspond to letter grades. Additionally, you can lock students out of assigments until they reach a specific level.</h2>
-        @if($levels)
+        @if(isNotEmpty($levels))
         <table class="table is-narrow">
             <thead>
             </thead>
@@ -42,31 +42,27 @@
         <div class="box">
 
             {!! Form::open(['url' => 'course/add/level', 'class' => '', 'id' => 'add-level']) !!}
-            <div class="field is-horizontal">
-              <div class="field-label is-normal">
-                <label class="label">Level Name</label>
-              </div>
-              <div class="field-body">
-                <div class="field is-grouped">
-                  <p class="control">
-                     {{ Form::input('text', 'level', null, ['class' => 'input', 'placeholder' => 'Newbie', 'id' => 'level_name']) }}
-                  </p>
-                </div>
-                <div class="field">
-                  <p class="control">
-                   {{ Form::input('number', 'amount', null, ['class' => 'input', 'placeholder' => '0', 'id' => 'level_amount']) }}
-                  </p>
-                  <div class="field-label is-normal">
-                    <label class="label">Points Required</label>
-                  </div>
-                </div>
-                <p class="control">
-                    {!! Form::submit('Add Level', ['class' => 'button is-primary is-medium']) !!}
-                </p>
-              </div>
+
+            <div class="field">
+              <label class="label">Level Name</label>
+              <p class="control">
+                {{ Form::input('text', 'level', null, ['class' => 'input', 'placeholder' => 'Newbie', 'id' => 'level_name']) }}
+              </p>
             </div>
-    
+
+            <div class="field">
+              <label class="label">Points Required</label>
+              <p class="control">
+                {{ Form::input('number', 'amount', null, ['class' => 'input', 'placeholder' => '0', 'id' => 'level_amount']) }}
+              </p>
+            </div>
+
+            <p class="control">
+                {!! Form::submit('Add Level', ['class' => 'button is-primary is-medium']) !!}
+            </p>
+
             {!! Form::close() !!}
+
         </div>
 
         {{ link_to('course/instructions', 'Finish!', ['class' => 'button is-large is-light']) }}
