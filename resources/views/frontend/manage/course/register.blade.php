56eeff7a54d13052e6f846f820c8eccf3b7335df
Clay Ewing <c.ewing@miami.edu>
2017-07-10 21:14:40 -0400

    form addon

--- resources/views/frontend/manage/course/register.blade.php
+++ resources/views/frontend/manage/course/register.blade.php
@@ -1,17 +1,24 @@
 @extends('frontend.layouts.unassigned')
 
 @section('content')
+
+<section class="hero is-dark is-bold is-large">
   <div class="hero-body">
     <div class="container is-fluid">
       <h1 class="title">
         Join a Course
       </h1>
       <h2 class="subtitle">To join a class, you should have been given a registration code. Please enter it below.</h2>
-        {!! Form::open(['url' => 'course/join', 'class' => '', 'id' => 'add-skill']) !!}
+        {!! Form::open(['url' => 'course/join', 'class' => '', 'id' => 'join-course']) !!}
             
+        <div class="field has-addons">
+          <p class="control">
             {{ Form::input('text', 'registration_code', null, ['class' => 'input is-large', 'placeholder' => 'Registration Code', 'id' => 'registration_code']) }}
 
-            {!! Form::submit('Join Course', ['class' => 'button is-large']) !!}
+          </p>
+          <p class="control">
+            {!! Form::submit('Join Course', ['class' => 'button is-large']) !!}  </p>
+        </div>
             
         {!! Form::close() !!}
     </div>
