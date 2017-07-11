9ccb36a75ef7d0052a883d150ba9ec4f759b3a39
Clay Ewing <c.ewing@miami.edu>
2017-07-11 11:40:56 -0400

    add tiles to new course page

--- resources/views/frontend/manage/course/create.blade.php
+++ resources/views/frontend/manage/course/create.blade.php
@@ -1,52 +1,22 @@
 @extends('frontend.layouts.unassigned')
 
 @section('content')
-<section class="hero is-dark is-bold is-large">
-  <div class="hero-body">
+<section class="section is-light is-large">
     <div class="container is-fluid">
       <h1 class="title">
         Create a Course
       </h1>
-      <h2 class="subtitle">Let's Get Started</h2>
         {!! Form::open(['url' => 'course/create', 'class' => '', 'id' => 'create-course']) !!}
 
+<div class="tile">
+  <div class="tile is-6 is-parent">
+    <div class="tile is-child box">
+        <p class="title">Course Information</p>
         <div class="field">
           <label class="label">Course Name</label>
           <p class="control">
             {{ Form::input('text', 'name', null, ['class' => 'input', 'placeholder' =>  'My New Course Name', 'id' => 'course_name']) }}
           </p>
-        </div>
-
-        <div class="field">
-            <label class="label">Instructor Display Name</label>
-            <p class="control">
-                {{ Form::input('text', 'instructor_display_name', null, ['class' => 'form-control', 'placeholder' =>  'Prof. Instructor', 'id' => 'instructor_name']) }}
-            </p>
-        </div>
-
-        <div class="field">
-            <label class="label">Instructor Contact Email</label>
-            <p class="control">
-                {{ Form::input('email', 'instructor_contact', null, ['class' => 'input', 'placeholder' =>  'professor.awesome@institution.edu', 'id' => 'instructor_contact']) }}
-            </p>
-        </div>
-
-        <div class="field">
-            <label class="label">Instructor Office Location</label>
-            <p class="control">
-                {{ Form::input('text', 'instructor_office_location', null, ['class' => 'form-control', 'placeholder' =>  'Fantastic HQ, Penthouse Suite', 'id' => 'office_location']) }}
-
-            </p>
-        </div>
-
-        <div class="field">
-            <label class="label">Instructor Office Hour</label>
-            <p class="control">
-                {{ Form::input('text', 'office_hours', null, ['class' => 'form-control', 'placeholder' =>  'Mondays at 8am', 'id' => 'office_hours']) }}
-            </p>
-        </div>
-
-
         <div class="field">
             <label class="label">Physical Location</label>
             <p class="control">
@@ -68,11 +38,50 @@
                 {{ Form::input('text', 'reg_code', null, ['class' => 'input', 'placeholder' => 'Unique Code for Students to Gain Access', 'id' => 'reg_code']) }}
             </p>
         </div>
-            <p class="control">
-                {!! Form::submit('Create Course', ['class' => 'button is-large is-primary']) !!}
-            </p>
-        {!! Form::close() !!}
+
         </div>
+    </div>
+  </div>
+  <div class="tile is-6 is-parent">
+    <div class="tile is-child box">
+        <p class="title">Instructor Information</p>
+        <div class="field">
+            <label class="label">Instructor Display Name</label>
+            <p class="control">
+                {{ Form::input('text', 'instructor_display_name', null, ['class' => 'input', 'placeholder' =>  'Prof. Instructor', 'id' => 'instructor_name']) }}
+            </p>
+        </div>
+
+        <div class="field">
+            <label class="label">Instructor Contact Email</label>
+            <p class="control">
+                {{ Form::input('email', 'instructor_contact', null, ['class' => 'input', 'placeholder' =>  'professor.instructor@institution.edu', 'id' => 'instructor_contact']) }}
+            </p>
+        </div>
+
+        <div class="field">
+            <label class="label">Instructor Office Location</label>
+            <p class="control">
+                {{ Form::input('text', 'instructor_office_location', null, ['class' => 'input', 'placeholder' =>  'Fantastic HQ, Penthouse Suite', 'id' => 'office_location']) }}
+
+            </p>
+        </div>
+
+        <div class="field">
+            <label class="label">Instructor Office Hours</label>
+            <p class="control">
+                {{ Form::input('text', 'office_hours', null, ['class' => 'input', 'placeholder' =>  'Mondays at 8am', 'id' => 'office_hours']) }}
+            </p>
+        </div>
+
+    </div>
+  </div>
+
+</div>
+    <p class="control">
+        {!! Form::submit('Create Course', ['class' => 'button is-large is-primary is-pulled-right']) !!}
+    </p>
+        {!! Form::close() !!}
     </div>
 </section>
 
