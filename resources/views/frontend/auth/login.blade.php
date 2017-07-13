417ff18c00fe653d75904cc897e7e84a4622da37
Clay Ewing <c.ewing@miami.edu>
2017-07-13 19:33:22 -0400

    duplicate login

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -1,76 +1,78 @@
 @extends('frontend.layouts.guest')
 
+@section('page-header')
+
+@endsection
+
+
 @section('content')
+<section class="hero is-dark is-medium">
+  <div class="hero-body">
+    <div class="container is-fluid">
+      <div class="tile">
+        <div class="tile is-6 is-parent">
+          <div class="tile is-child">
+            <img src="{!! URL::to('img/logo.png')!!}" alt="Queso: A Gameful Learning Management System"/>
 
-    <div class="row">
+              <p>Queso is a classroom management system for gameful classrooms. We help you take your existing classroom and reshape it using concepts from game design. This is not about points, badges, and leaderboards. This is about increasing student engagement through gameful design. You can use Queso for your class without "gamifying" it. It's simple and intuitive for creating an engaging class in the 21st century.</p>      
+          </div>
+        </div>
+        <div class="tile is-6 is-parent">
+          <div class="tile is-child box">
+            <p class="title">Login</p>
+              {{ Form::open(['route' => 'auth.login', 'class' => '']) }}
+              
+              <div class="field">
+                {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'label']) }}
+                <p class="control has-icons-left has-icons-right">
+                  {{ Form::input('email', 'email', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <i class="fa fa-warning"></i>
+                  </span>
+                </p>
+                <p class="help is-danger is-hidden">This email is invalid</p>
+              </div>
 
-        <div class="col-md-8 col-md-offset-2">
 
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('labels.frontend.auth.login_box_title') }}</div>
+              <div class="field">
+                {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'label']) }}
+                <p class="control has-icons-left has-icons-right">
+                  {{ Form::input('password', 'password', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.password')]) }}          
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-lock"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <i class="fa fa-warning"></i>
+                  </span>
+                </p>
+                <p class="help is-danger is-hidden">This email is invalid</p>
+              </div>
 
-                <div class="panel-body">
-
-                    {{ Form::open(['route' => 'auth.login', 'class' => 'form-horizontal']) }}
-
-                    <div class="form-group">
-                        {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
-
-                    <div class="form-group">
-                        {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
-
-                    @if (isset($captcha) && $captcha)
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                {!! Form::captcha() !!}
-                                {{ Form::hidden('captcha_status', 'true') }}
-                            </div><!--col-md-6-->
-                        </div><!--form-group-->
-                    @endif
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <div class="checkbox">
-                                <label>
-                                    {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
-                                </label>
-                            </div>
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
-
-                            {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
-
-                    {{ Form::close() }}
-
-                    <div class="row text-center">
-                        {!! $socialite_links !!}
+              <div class="field is-horizontal">
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <label class="checkbox">
+                          {{ Form::checkbox('remember') }} 
+                          {{ trans('labels.frontend.auth.remember_me') }}
+                      </label>
                     </div>
-                </div><!-- panel body -->
-
-            </div><!-- panel -->
-
-        </div><!-- col-md-8 -->
-
-    </div><!-- row -->
+                  </div>
+                </div>
+              </div>
+              {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'button is-primary']) }}
+              {{ Form::close() }}
+              <a href="{!! URL::to('password/reset')!!}" class="is-button is-link is-pulled-right">{!! trans('labels.frontend.passwords.forgot_password') !!}</a>
+    </div>
+  </div>
+</div>
 
 @endsection
 
 @section('after-scripts-end')
-    @if (isset($captcha) && $captcha)
-        {!! Captcha::script() !!}
-    @endif
+    <script>
+    </script>
 @stop
