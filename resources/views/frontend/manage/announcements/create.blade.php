23f6e5d9133bb3698d2b6cc4f0e75118b5aea306
Clay Ewing <c.ewing@miami.edu>
2017-07-16 17:52:00 -0400

    announcement create with sidebar

--- resources/views/frontend/manage/announcements/create.blade.php
+++ resources/views/frontend/manage/announcements/create.blade.php
@@ -2,37 +2,42 @@
 
 @section('content')
 
-<section class="hero is-dark is-bold">
-  <div class="hero-body">
-    <div class="container is-fluid">
-        {!! Form::open(['url' => 'manage/announcements/create', 'id'=>'quest-create-form']) !!}
-        {{ Form::hidden('course_id', $course_id, ['id' => 'course_id']) }}
-        <h1 class="title">
-        New Announcement
-      </h1>
-        <div class="field">
-          <p class="control">
-            {{ Form::input('text', 'title', null, ['class' => 'input', 'placeholder' => 'Adventure Awaits!', 'id' => 'headline']) }}
-          </p>
-        </div>
-        <div class="field">
-            <p class="control">
-            {!! Form::textarea('body', null, ['class' => 'input', 'files' => true]) !!}
-            </p>
-        </div>
-
-        <div class="field">
-            <p class="control">
-                {!! Form::checkbox('sticky', 1) !!} Show on Dashboard
-            </p>
-        </div>
-
-        {!! Form::submit('Post', ['class' => 'button is-primary is-large']) !!}
-
-
-        {!! Form::close() !!}
+<section class="section">
+<div class="columns">
+    <div class="column is-2">
+        @include('frontend.includes.admin')
     </div>
-  </div>
+    <div class="column">
+        <div class="container is-fluid">
+            {!! Form::open(['url' => 'manage/announcements/create', 'id'=>'quest-create-form']) !!}
+            {{ Form::hidden('course_id', $course_id, ['id' => 'course_id']) }}
+            <h1 class="title">
+            New Announcement
+          </h1>
+            <div class="field">
+              <p class="control">
+                {{ Form::input('text', 'title', null, ['class' => 'input', 'placeholder' => 'Adventure Awaits!', 'id' => 'headline']) }}
+              </p>
+            </div>
+            <div class="field">
+                <p class="control">
+                {!! Form::textarea('body', null, ['class' => 'input', 'files' => true]) !!}
+                </p>
+            </div>
+
+            <div class="field">
+                <p class="control">
+                    {!! Form::checkbox('sticky', 1) !!} Show on Dashboard
+                </p>
+            </div>
+
+            {!! Form::submit('Post', ['class' => 'button is-primary is-large']) !!}
+
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
 </section>
 
 @endsection
