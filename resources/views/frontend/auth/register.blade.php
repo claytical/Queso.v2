51b216d4257fb51874188847017357e0306ffd35
Clay Ewing <c.ewing@miami.edu>
2017-07-16 18:33:57 -0400

    reformat register page

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -1,56 +1,91 @@
 @extends('frontend.layouts.guest')
 
 @section('content')
-<h2>Register</h2>
+<section class="hero is-dark is-medium">
+    <div class="hero-body">
+        <div class="container is-fluid">
+        {{ Form::open(['route' => 'auth.register', 'class' => 'form-horizontal']) }}
 
-        <div class="col-md-12">
-                    {{ Form::open(['route' => 'auth.register', 'class' => 'form-horizontal']) }}
-                    <div class="form-group">
-                        {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('name', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+            <div class="tile">
+                <div class="tile is-6 is-parent is-vertical">
+                    <div class="tile is-child">
 
-                    <div class="form-group">
-                        {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                </div>
+                <div class="tile is-6 is-parent">
+                    <div class="tile is-child notification is-primary">
+                    <p class="title">Login</p>
+                      {{ Form::open(['route' => 'auth.login', 'class' => '']) }}
+                      
+                        <div class="field">                      
+                            {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'label']) }}
+                            <p class="control">
+                            {{ Form::input('name', 'name', null, ['class' => 'input is-large', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+                            </p>
+                        </div>
+                        <div class="field">
+                            {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'label']) }}
 
-                    <div class="form-group">
-                        {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                            <p class="control has-icons-left has-icons-right">
+                            {{ Form::input('email', 'email', null, ['class' => 'input is-large', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
 
-                    <div class="form-group">
-                        {{ Form::label('password_confirmation', trans('validation.attributes.frontend.password_confirmation'), ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
-                            {{ Form::input('password', 'password_confirmation', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password_confirmation')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                          <span class="icon is-small is-left">
+                            <i class="fa fa-envelope"></i>
+                          </span>
+                          <span class="icon is-small is-right">
+                            <i class="fa fa-warning"></i>
+                          </span>
+                        </p>
+                        <p class="help is-danger is-hidden">This email is invalid</p>
+                      </div>
+
+
+                      <div class="field">
+                        {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'label']) }}
+
+                        <p class="control has-icons-left has-icons-right">
+                          {{ Form::input('password', 'password', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.password')]) }}          
+                          <span class="icon is-small is-left">
+                            <i class="fa fa-lock"></i>
+                          </span>
+                          <span class="icon is-small is-right">
+                            <i class="fa fa-warning"></i>
+                          </span>
+                        </p>
+                      </div>
+
+                      <div class="field">
+                        {{ Form::label('password_confirmation', trans('validation.attributes.frontend.password_confirmation'), ['class' => 'label']) }}
+
+                        <p class="control has-icons-left has-icons-right">
+                          {{ Form::input('password', 'password_confirmation', null, ['class' => 'input', 'placeholder' => trans('validation.attributes.frontend.password_confirmation')]) }}          
+                          <span class="icon is-small is-left">
+                            <i class="fa fa-lock"></i>
+                          </span>
+                          <span class="icon is-small is-right">
+                            <i class="fa fa-warning"></i>
+                          </span>
+                        </p>
+                      </div>
 
                     @if (config('access.captcha.registration'))
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="field">
                                 {!! Form::captcha() !!}
                                 {{ Form::hidden('captcha_status', 'true') }}
-                            </div><!--col-md-6-->
                         </div><!--form-group-->
                     @endif
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {{ Form::submit(trans('labels.frontend.auth.register_button'), ['class' => 'btn btn-primary']) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
 
-                    {{ Form::close() }}
+                        {{ Form::submit(trans('labels.frontend.auth.register_button'), ['class' => 'button is-primary is-inverted is-outlined is-large']) }}
 
-        </div><!-- col-lg-12 -->
+                      {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 @endsection
 
 @section('after-scripts-end')
