4ac0f75cd86741b07eacac87e62d6bd2871cd55c
Clay Ewing <c.ewing@miami.edu>
2017-07-16 10:58:15 -0400

    update for link vs. content

--- resources/views/frontend/resources/course.blade.php
+++ resources/views/frontend/resources/course.blade.php
@@ -1,18 +1,47 @@
 @extends('frontend.layouts.master')
 
 @section('content')
+<section class="section">
+    <div class="columns is-multiline is-mobile">
+        @foreach($resources as $tag => $resource)
+            <div class="column is-half">
+                @if($tag)
+                    <p class="title">{!! $tag !!}</p>
+                @endif
+                @foreach($resource as $r)
+                    @if($r->link)
+                        <a href="{{ $r->link }}" data-iframely-url>{{ $r->link }}</a>
+                    @else
 
-<div class="columns is-multiline is-mobile">
-    @foreach($resources as $tag => $resource)
-        <div class="column is-half">
-            <p class="title">{!! $tag !!}</p>
-            @foreach($resource as $r)
-                {!! $r->title !!}
-            @endforeach
-        </div>
-    @endforeach
-</div>
+                        <article class="media">
+                          <div class="media-content">
+                            <div class="content">
+                              <p>
+                                <strong>{!! $r->title !!}</strong>
+                                <br>
+                                {!! $r->description !!}
+                              </p>
+                            </div>
+                            <nav class="level is-mobile">
+                              <div class="level-left">
+                                @if(!$r->files->isEmpty())
+                                    @foreach($r->files as $file)
+                                        <a class="level-item" href="{!! URL::to('uploads/' . $file->name) !!}" alt="{!! substr($file->name,5) !!}" download>
+                                        <span class="icon is-small"><i class="fa fa-paperclip"></i></span>
+                                        </a>
+                                    @endforeach
+                                @endif
+                              </div>
+                            </nav>
+                          </div>
 
+                        </article>
+                    @endif
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+</section>
 @endsection
 
 @section('after-scripts-end')
