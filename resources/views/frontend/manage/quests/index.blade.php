a4a09a29eaa993d3954dbdd4cfdb1646c12cb236
Clay Ewing <c.ewing@miami.edu>
2017-07-16 17:48:30 -0400

    reduce code

--- resources/views/frontend/manage/quests/index.blade.php
+++ resources/views/frontend/manage/quests/index.blade.php
@@ -32,23 +32,7 @@
 <section class="section">
     <div class="columns">
         <div class="column is-2">
-            <aside class="menu">
-              <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/quests/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course_id) !!}">Quests</a></li>
-                <li><a class="{{ Active::pattern('manage/resources/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course_id) !!}">Resources</a></li>
-                <li><a class="{{ Active::pattern('manage/announcements/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course_id) !!}">Announcements</a></li>
-              </ul>
-              <p class="menu-label">
-                Course Management
-              </p>
-              <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/course/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course_id) !!}">Information</a></li>
-                <li><a>Students</a></li>
-                <li><a>Teams</a></li>
-                <li><a>Skills</a></li>
-                <li><a>Levels</a></li>
-              </ul>
-            </aside>
+        @include('frontend.includes.admin')
         </div>
         <div class="column">
       <table class="table">
