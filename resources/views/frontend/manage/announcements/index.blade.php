ba43eb46f80805d2c3950ba1d2feb833edd7eac1
Clay Ewing <c.ewing@miami.edu>
2017-07-16 17:26:13 -0400

    update course id ref

--- resources/views/frontend/manage/announcements/index.blade.php
+++ resources/views/frontend/manage/announcements/index.blade.php
@@ -35,15 +35,15 @@
         <div class="column is-2">
             <aside class="menu">
               <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/quests/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course->id) !!}">Quests</a></li>
-                <li><a class="{{ Active::pattern('manage/resources/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course->id) !!}">Resources</a></li>
-                <li><a class="{{ Active::pattern('manage/announcements/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course->id) !!}">Announcements</a></li>
+                <li><a class="{{ Active::pattern('manage/quests/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course_id) !!}">Quests</a></li>
+                <li><a class="{{ Active::pattern('manage/resources/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course_id) !!}">Resources</a></li>
+                <li><a class="{{ Active::pattern('manage/announcements/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course_id) !!}">Announcements</a></li>
               </ul>
               <p class="menu-label">
                 Course Management
               </p>
               <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/course/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course->id) !!}">Information</a></li>
+                <li><a class="{{ Active::pattern('manage/course/'.$course_id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course_id) !!}">Information</a></li>
                 <li><a>Students</a></li>
                 <li><a>Teams</a></li>
                 <li><a>Skills</a></li>
