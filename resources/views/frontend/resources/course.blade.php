6849436ee9115c9ab500fce6250263680635e63a
Clay Ewing <c.ewing@miami.edu>
2017-07-16 10:12:22 -0400

    dump resources

--- resources/views/frontend/resources/course.blade.php
+++ resources/views/frontend/resources/course.blade.php
@@ -3,14 +3,7 @@
 @section('content')
 
 <div class="columns is-multiline is-mobile">
-    @foreach($resources as $category => $resource)
-        <div class="column is-half">
-            <p class="title">{!! $category !!}</p>
-            @foreach($resource as $category => $r)
-                {!! $r->title !!}
-            @endforeach
-        </div>
-    @endforeach
+{!! var_dump($resources) !!}
 </div>
 
 @endsection
