ff6bfa2f7aa29476158f7e41921111e81737706e
Clay Ewing <c.ewing@miami.edu>
2017-07-15 20:57:36 -0400

    relabel button

--- resources/views/frontend/manage/resources/edit/content.blade.php
+++ resources/views/frontend/manage/resources/edit/content.blade.php
@@ -90,7 +90,7 @@
         <div class="msf-navigation">
             <button data-type="back" class="button is-large msf-nav-button" type="button">Previous</button>
             <button data-type="next" class="button is-large msf-nav-button" type="button">Next</button>
-            <button data-type="submit" class="button msf-nav-button is-primary is-large" type="submit">Create Content</button>
+            <button data-type="submit" class="button msf-nav-button is-primary is-large" type="submit">Update Content</button>
         </div>
     </div>
   </div>
