f617676bdf8681786cbccd8bd5981531083423f0
Clay Ewing <c.ewing@miami.edu>
2017-07-16 17:23:03 -0400

    add menu to index pages

--- resources/views/frontend/manage/announcements/index.blade.php
+++ resources/views/frontend/manage/announcements/index.blade.php
@@ -31,6 +31,28 @@
 
 @if(!$announcements->isEmpty())
 <section class="section">
+    <div class="columns">
+        <div class="column is-2">
+            <aside class="menu">
+              <ul class="menu-list">
+                <li><a class="{{ Active::pattern('manage/quests/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course->id) !!}">Quests</a></li>
+                <li><a class="{{ Active::pattern('manage/resources/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course->id) !!}">Resources</a></li>
+                <li><a class="{{ Active::pattern('manage/announcements/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course->id) !!}">Announcements</a></li>
+              </ul>
+              <p class="menu-label">
+                Course Management
+              </p>
+              <ul class="menu-list">
+                <li><a class="{{ Active::pattern('manage/course/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course->id) !!}">Information</a></li>
+                <li><a>Students</a></li>
+                <li><a>Teams</a></li>
+                <li><a>Skills</a></li>
+                <li><a>Levels</a></li>
+              </ul>
+            </aside>
+        </div>
+        <div class="column">
+
     <table class="table">
       <thead>
         <tr>
@@ -69,6 +91,8 @@
 
       </tbody>
     </table>
+    </div>
+  </div>
 </section>
 @endif
 
--- resources/views/frontend/manage/quests/index.blade.php
+++ resources/views/frontend/manage/quests/index.blade.php
@@ -30,85 +30,102 @@
 
 @if(!$quests->isEmpty())
 <section class="section">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Quest</th>
-          <th>Type</th>
-          <th>Due Date</th>
-          <th></th>
-        </tr>
-      </thead>
-      <!--
-      <tfoot>
-        <tr>
-          <th>Headline</th>
-          <th>Date</th>
-          <th></th>
-        </tr>
-      </tfoot>
-      -->
-      <tbody>
-    @foreach($quests as $quest)
-        <tr>
-          <td>{!! $quest->name !!}</td>
-          <td>
-              @if($quest->quest_type_id == 1)
-                Response
-              @endif
-              @if($quest->quest_type_id == 2)
-                Activity
-              @endif
-              @if($quest->quest_type_id == 3)
-                Video
-              @endif
-              @if($quest->quest_type_id == 4)
-                Link
-              @endif            
-              @if($quest->quest_type_id == 5)
-                File Upload
-              @endif            
-              @if($quest->quest_type_id == 6)
-                Group File Upload
-              @endif            
-              @if($quest->quest_type_id == 7)
-                Group Link
-              @endif            
+    <div class="columns">
+        <div class="column is-2">
+            <aside class="menu">
+              <ul class="menu-list">
+                <li><a class="{{ Active::pattern('manage/quests/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course->id) !!}">Quests</a></li>
+                <li><a class="{{ Active::pattern('manage/resources/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course->id) !!}">Resources</a></li>
+                <li><a class="{{ Active::pattern('manage/announcements/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course->id) !!}">Announcements</a></li>
+              </ul>
+              <p class="menu-label">
+                Course Management
+              </p>
+              <ul class="menu-list">
+                <li><a class="{{ Active::pattern('manage/course/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course->id) !!}">Information</a></li>
+                <li><a>Students</a></li>
+                <li><a>Teams</a></li>
+                <li><a>Skills</a></li>
+                <li><a>Levels</a></li>
+              </ul>
+            </aside>
+        </div>
+        <div class="column">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Quest</th>
+            <th>Type</th>
+            <th>Due Date</th>
+            <th></th>
+          </tr>
+        </thead>
+        <!--
+        <tfoot>
+          <tr>
+            <th>Headline</th>
+            <th>Date</th>
+            <th></th>
+          </tr>
+        </tfoot>
+        -->
+        <tbody>
+      @foreach($quests as $quest)
+          <tr>
+            <td>{!! $quest->name !!}</td>
+            <td>
+                @if($quest->quest_type_id == 1)
+                  Response
+                @endif
+                @if($quest->quest_type_id == 2)
+                  Activity
+                @endif
+                @if($quest->quest_type_id == 3)
+                  Video
+                @endif
+                @if($quest->quest_type_id == 4)
+                  Link
+                @endif            
+                @if($quest->quest_type_id == 5)
+                  File Upload
+                @endif            
+                @if($quest->quest_type_id == 6)
+                  Group File Upload
+                @endif            
+                @if($quest->quest_type_id == 7)
+                  Group Link
+                @endif            
 
-          </td>
-          <td>
-            @if($quest->expires_at)
-            {!! date('m-d-Y', strtotime($quest->expires_at)) !!}
-            @else
-              Anytime
-            @endif
-          </td>
-          <td>
-            <a class="button is-small" href="{!! url('manage/quest/'.$quest->id);!!}">Edit</a>
-            <a class="button is-small" href="{!! url('quest/'.$quest->id.'/attempt/submission');!!}">View</a>
-            <a class="button is-small" href="{!! url('manage/quest/'.$quest->id.'/clone');!!}">Clone</a>
-
-            @if($quest->instant)
-              <a class="button is-small" href="{!! url('manage/quest/'.$quest->id.'/qrcards');!!}">QR Code Sheet</a>
-            @endif
-            <a class="button is-small is-danger" href="{!! url('manage/quest/'.$quest->id.'/delete');!!}">Delete</a>
             </td>
-        </tr>
-     @endforeach
+            <td>
+              @if($quest->expires_at)
+              {!! date('m-d-Y', strtotime($quest->expires_at)) !!}
+              @else
+                Anytime
+              @endif
+            </td>
+            <td>
+              <a class="button is-small" href="{!! url('manage/quest/'.$quest->id);!!}">Edit</a>
+              <a class="button is-small" href="{!! url('quest/'.$quest->id.'/attempt/submission');!!}">View</a>
+              <a class="button is-small" href="{!! url('manage/quest/'.$quest->id.'/clone');!!}">Clone</a>
 
-      </tbody>
-    </table>
+              @if($quest->instant)
+                <a class="button is-small" href="{!! url('manage/quest/'.$quest->id.'/qrcards');!!}">QR Code Sheet</a>
+              @endif
+              <a class="button is-small is-danger" href="{!! url('manage/quest/'.$quest->id.'/delete');!!}">Delete</a>
+              </td>
+          </tr>
+       @endforeach
+
+        </tbody>
+      </table>
+    </div>
+  </div>
 </section>
 @endif
 
 @endsection
 
 @section('after-scripts-end')
-    <script>
-  /*      var quest_list_options = {
-        valueNames: [ 'submission', 'date', 'student' ]
-    };
-*/
-//    var hackerList = new List('submission-list', submission_list_options);
-    </script>
+
 @stop
