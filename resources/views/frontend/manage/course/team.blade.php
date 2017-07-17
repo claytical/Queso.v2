19fce99d78ad921fd63ce1c9291309bef1dc85a2
Clay Ewing <c.ewing@miami.edu>
2017-07-17 14:56:20 -0400

    update team blade

--- resources/views/frontend/manage/course/team.blade.php
+++ resources/views/frontend/manage/course/team.blade.php
@@ -1,51 +1,61 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-    <h2>{!! $team->name !!}</h2>
-{!! Form::open(array('url' => 'manage/course/team')) !!}
-{!! Form::hidden('team_id', $team->id ) !!}
+<section class="section">
+    <div class="columns">
+        <div class="column is-2">
+        @include('frontend.includes.admin')
+        </div>
+        <div class="column">
+            {!! Form::open(array('url' => 'manage/course/team')) !!}
+            {!! Form::hidden('team_id', $team->id ) !!}
+            <select class="input is-large" multiple="multiple" name="from" id="add_to_team">
+                    @foreach($students_not_on_team as $student)
+                        <option value="{!! $student->id !!}">{!! $student->name !!}</option>
+                    @endforeach
+            </select>
 
-    <div class="col-xs-5">
-        <b>Not on Team</b>
-        <select name="from[]" id="multiselect" class="form-control" size="8" multiple="multiple">
-        @foreach($students_not_on_team as $student)
-            <option value="{!! $student->id !!}">{!! $student->name !!}</option>
-        @endforeach
-        </select>
-    </div>
-    
-    <div class="col-xs-2">
-    &nbsp;
-        <button type="button" id="multiselect_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
-        <button type="button" id="multiselect_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
-        <button type="button" id="multiselect_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
-        <button type="button" id="multiselect_leftAll" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
-    </div>
-    
-    <div class="col-xs-5">
-        <b>On Team</b>
-        <select name="to[]" id="multiselect_to" class="form-control" size="8" multiple="multiple">
-            @foreach($students as $student)
-                <option value="{!! $student->id !!}">{!! $student->name !!}</option>
-            @endforeach
+            {!! Form::submit('Add to Team', ['class' => 'button is-large is-primary is-pulled-right']) !!}
             
-        </select>
-    </div>
+            {!! Form::close() !!}
 
-<div class="col-lg-12">
-    <hr/>
-    {!! Form::submit('Set Team', ['class' => 'btn btn-primary btn-lg btn-block']) !!}
-</div>
-    
-    {!! Form::close() !!}
+            <h1 class="title">{!! $team->name !!}</h1>
+
+            @if(!$team->isEmpty())
+
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Email Address</th>
+                      <th class="grades">Points</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($students as $student)
+                        <tr>
+                            <td>{{ link_to('manage/student/'.$student->id, $student->name) }}</td>
+                            <td><a href="mailto:{!! $student->email !!}">{!! $student->email !!}</td>
+                            <td class="grades">{!! $student->skills()->where('course_id', '=', $course_id)->sum('amount') !!}</td>
+                            <td>{{ link_to('#', '', ['class' => 'delete']) }}</td>
+                        </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+            @else
+                <p class="subtitle">There are currently no students on this team.</p>
+            @endif
+        </div>
+      </div>
+</section>
 
 @endsection
 
 @section('after-scripts-end')
     <script>
-
-    jQuery(document).ready(function($) {
-        $('#multiselect').multiselect();
+    $("#add_to_team").multipleSelect({
+        placeholder: "Select Students"
     });
 
 
