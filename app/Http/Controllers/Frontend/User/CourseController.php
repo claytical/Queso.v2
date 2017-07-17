f8f0a0aad8e9be8191ec2da8f7c806027c71a847
Clay Ewing <c.ewing@miami.edu>
2017-07-17 11:42:59 -0400

    add course_id to skills management

--- app/Http/Controllers/Frontend/User/CourseController.php
+++ app/Http/Controllers/Frontend/User/CourseController.php
@@ -379,7 +379,7 @@ class CourseController extends Controller
 //        $levels = $course->levels()->orderBy('amount', 'desc')->get();
 //        $teams = $course->teams;
  //       $tzlist = \DateTimeZone::listIdentifiers(\DateTimeZone::ALL);
-        return view('frontend.manage.course.edit.skills', ['course' => $course, 'skills' => $skills])
+        return view('frontend.manage.course.edit.skills', ['course' => $course, 'skills' => $skills, 'course_id' => $course_id])
             ->withUser(access()->user());
     }
 
