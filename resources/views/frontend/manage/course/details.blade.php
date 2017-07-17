622434dc4131e5faa10daf9e24de9512bf00ecc8
Clay Ewing <c.ewing@miami.edu>
2017-07-17 12:38:37 -0400

    update admin menu placement

--- resources/views/frontend/manage/course/details.blade.php
+++ resources/views/frontend/manage/course/details.blade.php
@@ -6,23 +6,8 @@
 
     <div class="columns">
         <div class="column is-2">
-            <aside class="menu">
-              <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/quests/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/quests/'.$course->id) !!}">Quests</a></li>
-                <li><a class="{{ Active::pattern('manage/resources/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/resources/'.$course->id) !!}">Resources</a></li>
-                <li><a class="{{ Active::pattern('manage/announcements/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/announcements/'.$course->id) !!}">Announcements</a></li>
-              </ul>
-              <p class="menu-label">
-                Course Management
-              </p>
-              <ul class="menu-list">
-                <li><a class="{{ Active::pattern('manage/course/'.$course->id, 'is-active') }}" href="{!! URL::to('manage/course/'.$course->id) !!}">Information</a></li>
-                <li><a>Students</a></li>
-                <li><a>Teams</a></li>
-                <li><a>Skills</a></li>
-                <li><a>Levels</a></li>
-              </ul>
-            </aside>
+            @include('frontend.includes.admin')
+
         </div>
         <div class="column">
         {!! Form::open(['url' => 'manage/course/update', 'id'=>'resource-form', 'class' => '']) !!}
