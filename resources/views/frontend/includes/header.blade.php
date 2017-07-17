721440e465b94b1a10885bf8260aea20426a61b3
Clay Ewing <c.ewing@miami.edu>
2017-07-17 11:00:54 -0400

    update header

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -31,20 +31,6 @@
                         </div>
                 </div>
 
-                <div class="navbar-item has-dropdown is-hoverable">                
-                        <a class="navbar-link  is-active" href="#">
-                          Grade
-                        </a>
-                        <div class="navbar-dropdown is-boxed">
-                          @foreach(access()->courses() as $c)
-
-                            <a class="navbar-item {{ Active::pattern('grade/activity/select/'.$c->id, 'is-active') }}" href="{!! URL::to('grade/activity/select/'.$c->id) !!}">
-                              {!! $c->name !!}
-                            </a>
-                          @endforeach
-                        </div>
-                </div>
-
                 <div class="navbar-item has-dropdown is-hoverable">
                         <a class="navbar-link  is-active" href="#">
                           Manage
