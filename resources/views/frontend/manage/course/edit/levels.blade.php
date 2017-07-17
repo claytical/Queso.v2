564b24c642ef9a9e375443cfc47dc8ade8bd74d2
Clay Ewing <c.ewing@miami.edu>
2017-07-17 12:53:38 -0400

    update language

--- resources/views/frontend/manage/course/edit/levels.blade.php
+++ resources/views/frontend/manage/course/edit/levels.blade.php
@@ -10,27 +10,24 @@
             <h1 class="title">Levels</h1>
                 {!! Form::open(['url' => 'course/add/level', 'class' => '', 'id' => 'add-level']) !!}
               <div class="field is-horizontal">
-                <div class="field-label is-normal">
-                  <label class="label">Level Name</label>
-                </div>
                 <div class="field-body">
                   <div class="field is-grouped">
                     <p class="control is-expanded">
-                    {{ Form::input('text', 'level', null, ['class' => 'input is-large', 'placeholder' => 'Name', 'id' => 'level_name']) }}
+                    {{ Form::input('text', 'level', null, ['class' => 'input is-large', 'placeholder' => 'Level Name', 'id' => 'level_name']) }}
                     </p>
                   </div>
                   <div class="field">
                     <p class="control is-expanded">
-                      {{ Form::input('number', 'amount', null, ['class' => 'input is-large', 'placeholder' => '0', 'id' => 'level_amount']) }}                      
+                      {{ Form::input('number', 'amount', null, ['class' => 'input is-large', 'placeholder' => 'Points Required', 'id' => 'level_amount']) }}                      
                     </p>
                   </div>
+                {!! Form::submit('Add Level', ['class' => 'button is-primary is-large']) !!}
+
                 </div>
               </div>
             <p class="control">
                 {{ Form::hidden('course_exists', true, ['id' => 'course_check']) }}
                 {{ Form::hidden('course_id', $course_id, ['id' => 'course_id']) }}
-
-                {!! Form::submit('Add Level', ['class' => 'button is-primary is-medium']) !!}
             </p>
 
             {!! Form::close() !!}
