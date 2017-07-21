f8a5d3e354a57c73bb0b8a4e4bcf80138d9195b7
Clay Ewing <c.ewing@miami.edu>
2017-07-21 17:51:14 -0400

    update structure of response

--- resources/views/frontend/quests/attempt/response.blade.php
+++ resources/views/frontend/quests/attempt/response.blade.php
@@ -21,36 +21,38 @@
                 <div class="tile is-1"></div>
                 <div class="tile is-4">
                     <div class="tile is-child">
-                    @if($quest->expires_at)
-                        <h4 class="title">Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
-                    @endif
+                        @if($quest->expires_at)
+                            <h4 class="title">Due {!! date('m-d-Y', strtotime($quest->expires_at)) !!}</h4>
+                        @endif
 
-                    @if(!$files->isEmpty())
-                        <p class="subtitle">Attached Files</p>
+                        @if(!$files->isEmpty())
+                            <p class="subtitle">Attached Files</p>
 
-                        @foreach($files as $file)
-                            <a class="level-item" href="{!! URL::to('uploads/' . $file->name) !!}" title="{!! substr($file->name,5) !!}" download>
-                            <span class="icon is-small"><i class="fa fa-paperclip"></i></span> 
-                            {!! substr($file->name,5) !!}
-                            </a>
-                        @endforeach
-                    @endif
-                    <h3 class="subtitle">{!! $quest->skills()->sum('amount') !!} Points Available</h3>
-                        @foreach($skills as $skill)
-                            <p>{!! $skill->name !!} <span class="is-pulled-right">{!! $skill->pivot->amount !!}</span></p>
-                       @endforeach
-                    <div class="field">
-                        <p class="control">
-                        {!! Form::submit('Submit', ['class' => 'button is-primary is-large is-fullwidth']) !!}
-                        </p>
+                            @foreach($files as $file)
+                                <a class="level-item" href="{!! URL::to('uploads/' . $file->name) !!}" title="{!! substr($file->name,5) !!}" download>
+                                <span class="icon is-small"><i class="fa fa-paperclip"></i></span> 
+                                {!! substr($file->name,5) !!}
+                                </a>
+                            @endforeach
+                        @endif
+                        <h3 class="subtitle">{!! $quest->skills()->sum('amount') !!} Points Available</h3>
+                            @foreach($skills as $skill)
+                                <p>{!! $skill->name !!} <span class="is-pulled-right">{!! $skill->pivot->amount !!}</span></p>
+                           @endforeach
                     </div>
-
+                    <div class="tile is-child">
+                        <div class="field">
+                            <p class="control">
+                            {!! Form::submit('Submit', ['class' => 'button is-primary is-large is-fullwidth']) !!}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-            {!! Form::close() !!}
     </div>
+            {!! Form::close() !!}
+</div>
 </section>
 
 @endsection
