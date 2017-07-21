8d8dd165dc5cce865a1780ae2dc65cf319a4358d
Clay Ewing <c.ewing@miami.edu>
2017-07-21 16:45:42 -0400

    video create style

--- resources/views/frontend/manage/quests/create/video.blade.php
+++ resources/views/frontend/manage/quests/create/video.blade.php
@@ -11,100 +11,84 @@
                     {{ Form::hidden('instant', false, ['id' => 'instant_allowed']) }}
 
 
-<section class="hero" id="choose_quest">
-  <div class="hero-body">
-    <div class="container is-fluid">
-        <div class="msf-header">
-          <div class="has-text-centered">
-            <div class="columns">
-              <div class="msf-step column"><i class="fa fa-info"></i> <p>Information</p></div>
-              <div class="msf-step column"><i class="fa fa-trophy"></i><p>Skills</p></div>
+<section class="section section-dark" id="choose_quest">
+  <div class="columns">
+    <div class="column is-2">
+            @include('frontend.includes.admin')
+    </div>
+    <div class="column">
+      <div class="box">
 
-            </div>
-          </div>
-        </div>
-        <div class="msf-content">
-        <div class="msf-view">
-            <div class="tile">
-                <div class="tile is-8 is-parent">
-                  <div class="tile is-child">
-                  <!-- Title and Description -->                
-                    <div class="field">
-                      <p class="control">
-                        {{ Form::input('text', 'name', null, ['class' => 'input is-large', 'placeholder' => 'Quest Title', 'id' => 'quest_title']) }}
-                      </p>
-                    </div>
+        <div class="container is-fluid">
+            <div class="msf-header">
+              <div class="has-text-centered">
+                <div class="columns">
+                  <div class="msf-step column"><i class="fa fa-info"></i> <p>Information</p></div>
+                  <div class="msf-step column"><i class="fa fa-trophy"></i><p>Skills</p></div>
 
-                    <div class="field">
-                      <p class="control">
-                        {!! Form::textarea('description', null, ['class' => 'input', 'placeholder' => 'Enter an explanation or instructions for the quest here...', 'files' => false, 'id' => 'description']) !!}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="tile is-4 is-parent">
-                    <div class="tile is-child notification">
-                      <h4 class="subtitle">YouTube URL</h4>
-                        <div class="field">
-                          <p class="control">
-                            {{ Form::input('text', 'video_url', null, ['class' => 'input', 'placeholder' => 'http://youtube.com/watch/?v=AAAAAAA', 'id' => 'video_url']) }}
-
-                          </p>
-                        </div>
-                      <h4 class="subtitle">Due Date</h4>
-                        <div class="field">
-                          <p class="control">
-                            <label class="radio">
-                              <input type="radio" name="expires" value="0" checked>
-                              Anytime
-                            </label>
-                            <label class="radio">
-                              <input type="radio" name="expires" value="1">
-                              Specific Date
-                            </label>
-                          </p>
-                        </div>
-                    </div>
                 </div>
               </div>
             </div>
+            <div class="msf-content">
             <div class="msf-view">
+                <div class="tile">
+                    <div class="tile is-8 is-parent">
+                      <div class="tile is-child">
+                      <!-- Title and Description -->                
+                        <div class="field">
+                          <p class="control">
+                            {{ Form::input('text', 'name', null, ['class' => 'input is-large', 'placeholder' => 'Quest Title', 'id' => 'quest_title']) }}
+                          </p>
+                        </div>
 
-              <div class="tile is-6 is-parent">
-                  <div class="container is-fluid">
-                      <p>You can set a maximum amount of points you're able to award to a student for completing this quest for each skill.</p>
-                  </div>
-              </div>
+                        <div class="field">
+                          <p class="control">
+                            {!! Form::textarea('description', null, ['class' => 'input', 'placeholder' => 'Enter an explanation or instructions for the quest here...', 'files' => false, 'id' => 'description']) !!}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
 
-              <div class="tile">
-                  <div class="tile is-6 is-parent">
-                    <div class="tile is-child">
-                    <!-- Skills -->            
-                      <h4 class="subtitle has-text-centered">Points Awarded</h4>
+                    <div class="tile is-4 is-parent">
+                        <div class="tile is-child notification">
+                          <h4 class="subtitle">YouTube URL</h4>
+                            <div class="field">
+                              <p class="control">
+                                {{ Form::input('text', 'video_url', null, ['class' => 'input', 'placeholder' => 'http://youtube.com/watch/?v=AAAAAAA', 'id' => 'video_url']) }}
 
-                      @foreach($skills as $skill)
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">{!! $skill->name !!}</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field is-grouped">
-                              <p class="control is-expanded has-icons-left">
-                                <input class="input is-large" type="number" name="skill[]" placeholder="Maximum Points">
-                                <input type="hidden" name="skill_id[]" class="skills-input" value={!! $skill->id !!}>
                               </p>
                             </div>
-                          </div>
+                          <h4 class="subtitle">Due Date</h4>
+                            <div class="field">
+                              <p class="control">
+                                <label class="radio">
+                                  <input type="radio" name="expires" value="0" checked>
+                                  Anytime
+                                </label>
+                                <label class="radio">
+                                  <input type="radio" name="expires" value="1">
+                                  Specific Date
+                                </label>
+                              </p>
+                            </div>
                         </div>
-                    
-                      @endforeach
-
                     </div>
                   </div>
+                </div>
+                <div class="msf-view">
+
                   <div class="tile is-6 is-parent">
-                      <div class="tile is-child">
-                        <h4 class="subtitle has-text-centered">Minimum Skill Level Required</h4>
+                      <div class="container is-fluid">
+                          <p>You can set a maximum amount of points you're able to award to a student for completing this quest for each skill.</p>
+                      </div>
+                  </div>
+
+                  <div class="tile">
+                      <div class="tile is-6 is-parent">
+                        <div class="tile is-child">
+                        <!-- Skills -->            
+                          <h4 class="subtitle has-text-centered">Points Awarded</h4>
+
                           @foreach($skills as $skill)
                             <div class="field is-horizontal">
                               <div class="field-label is-normal">
@@ -113,31 +97,54 @@
                               <div class="field-body">
                                 <div class="field is-grouped">
                                   <p class="control is-expanded has-icons-left">
-                                    <input class="input is-large" name="threshold[]" type="number" placeholder="Maximum Points">
-                                    <input type="hidden" name="threshold_skill_id[]" class="thresholds-input" value="{!! $skill->id !!}">
+                                    <input class="input is-large" type="number" name="skill[]" placeholder="Maximum Points">
+                                    <input type="hidden" name="skill_id[]" class="skills-input" value={!! $skill->id !!}>
                                   </p>
                                 </div>
                               </div>
                             </div>
+                        
+                          @endforeach
 
-                          @endforeach                       
-                     
+                        </div>
                       </div>
+                      <div class="tile is-6 is-parent">
+                          <div class="tile is-child">
+                            <h4 class="subtitle has-text-centered">Minimum Skill Level Required</h4>
+                              @foreach($skills as $skill)
+                                <div class="field is-horizontal">
+                                  <div class="field-label is-normal">
+                                    <label class="label">{!! $skill->name !!}</label>
+                                  </div>
+                                  <div class="field-body">
+                                    <div class="field is-grouped">
+                                      <p class="control is-expanded has-icons-left">
+                                        <input class="input is-large" name="threshold[]" type="number" placeholder="Maximum Points">
+                                        <input type="hidden" name="threshold_skill_id[]" class="thresholds-input" value="{!! $skill->id !!}">
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+
+                              @endforeach                       
+                         
+                          </div>
+                      </div>
+                    </div>
+
+                    </div>
+
                   </div>
-                </div>
-
-                </div>
-
+              </div>
+              <div class="msf-navigation">
+                    <button data-type="back" class="button is-large msf-nav-button" type="button">Previous</button>
+                    <button data-type="next" class="button is-large msf-nav-button" type="button">Next</button>
+                    <button data-type="submit" class="button msf-nav-button is-primary is-large" type="submit">Create Quest</button>
               </div>
           </div>
-          <div class="msf-navigation">
-                <button data-type="back" class="button is-large msf-nav-button" type="button">Previous</button>
-                <button data-type="next" class="button is-large msf-nav-button" type="button">Next</button>
-                <button data-type="submit" class="button msf-nav-button is-primary is-large" type="submit">Create Quest</button>
-          </div>
+        </div>
     </div>
-  </div>
-</section>
+  </section>
       {!! Form::close() !!}
 
 
