76956718aad7894a57f6dba37230d32683f4e7f0
Clay Ewing <c.ewing@miami.edu>
2017-07-24 14:58:27 -0400

    update video form

--- resources/views/frontend/manage/quests/create/video.blade.php
+++ resources/views/frontend/manage/quests/create/video.blade.php
@@ -90,13 +90,13 @@
                           <h4 class="subtitle has-text-centered">Points Awarded</h4>
 
                           @foreach($skills as $skill)
-                            <div class="field is-horizontal">
-                              <div class="field-label is-normal">
+                            <div class="field">
+                              <div class="field-label">
                                 <label class="label">{!! $skill->name !!}</label>
                               </div>
                               <div class="field-body">
-                                <div class="field is-grouped">
-                                  <p class="control has-icons-left">
+                                <div class="field">
+                                  <p class="control is-expanded">
                                     <input class="input is-large" type="number" name="skill[]" placeholder="Maximum Points">
                                     <input type="hidden" name="skill_id[]" class="skills-input" value={!! $skill->id !!}>
                                   </p>
@@ -112,13 +112,13 @@
                           <div class="tile is-child">
                             <h4 class="subtitle has-text-centered">Minimum Skill Level Required</h4>
                               @foreach($skills as $skill)
-                                <div class="field is-horizontal">
-                                  <div class="field-label">
+                                <div class="field">
+                                  <div class="field">
                                     <label class="label">{!! $skill->name !!}</label>
                                   </div>
                                   <div class="field-body">
                                     <div class="field">
-                                      <p class="control">
+                                      <p class="control is-expanded">
                                         <input class="input is-large" name="threshold[]" type="number" placeholder="Maximum Points">
                                         <input type="hidden" name="threshold_skill_id[]" class="thresholds-input" value="{!! $skill->id !!}">
                                       </p>
