529965552c55e779b2ba48c8e72b86d273fc401e
Clay Ewing <c.ewing@miami.edu>
2017-08-22 14:52:56 -0400

    skill check

--- resources/views/frontend/quests/history.blade.php
+++ resources/views/frontend/quests/history.blade.php
@@ -263,10 +263,12 @@
 
                 @if($skills)
                     @if(count($skills) > 1)
+                        @if($skill)
                         @foreach($skills as $skill)
                                 <p><strong>{!! $skill['name'] !!}</strong> <span class="is-pulled-right">{!! $skill['amount'] !!}</span></p>
                         @endforeach
                         <hr/>
+                        @endif
                         <p><strong>Total</strong> <span class="is-pulled-right">{!! $total_points !!}</span></p>
                     @else
                             <p><strong>{!! $skill[0]['name'] !!}</strong> <span class="is-pulled-right">{!! $skill[0]['amount'] !!}</span></p>
